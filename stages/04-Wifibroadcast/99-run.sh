97740909f9d5d5c9f27c10e361cc5d67c39c226d
root <yes21@canopeo.pro>
2019-01-26 16:12:36 +0100

    Changing FAT partition label from WIFIBROADCAST to OPENHD

--- stages/04-Wifibroadcast/99-run.sh
+++ stages/04-Wifibroadcast/99-run.sh
@@ -8,7 +8,7 @@ sudo cp "$MNT_DIR/root/ld.so.preload" "$MNT_DIR/etc/ld.so.preload"
 BOOT_MNT_DIR="${STAGE_WORK_DIR}/mnt/boot"
 BOOT_LOOP_DEV="$(findmnt -nr -o source $BOOT_MNT_DIR)"
 
-fatlabel "$BOOT_LOOP_DEV" "WIFIBRDCAST"
+fatlabel "$BOOT_LOOP_DEV" "OPENHD"
 
 #return
 popd
