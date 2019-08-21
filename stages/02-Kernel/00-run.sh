c556efb1fa37989b5f92e84c5be84470a3785b38
Stephen Oliver <steve@infincia.com>
2019-08-21 03:07:44 -0400

    dont delete kernel source every build, just reset tree

--- stages/02-Kernel/00-run.sh
+++ stages/02-Kernel/00-run.sh
@@ -3,13 +3,15 @@ set -e
 # Do this to the WORK folder of this stage
 pushd ${STAGE_WORK_DIR}
 
-log "Remove previous Kernel"
-rm -r linux || true
-
-log "Download the Raspberry Pi Kernel"
-git clone --depth=100 -b ${PI_KERNEL_BRANCH} ${PI_KERNEL_REPO}
+if [ ! -d "linux" ]; then
+    log "Download the Raspberry Pi Kernel"
+    git clone --depth=100 -b ${PI_KERNEL_BRANCH} ${PI_KERNEL_REPO}
+fi
 
 pushd linux
+git reset --hard
+git pull
+
 # Switch to specific commit
 git checkout $GIT_KERNEL_SHA1
 
