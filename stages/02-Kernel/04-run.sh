9e801dc80131f690d127f23b92a406b7edf5bb21
Stephen Oliver <steve@infincia.com>
2020-03-22 00:29:20 -0400

    Only build pi v7l kernel on buster

--- stages/02-Kernel/04-run.sh
+++ stages/02-Kernel/04-run.sh
@@ -1,34 +1,39 @@
 set -e
 
-# Do this to the WORK folder of this stage
-pushd ${STAGE_WORK_DIR}
-MNT_DIR="${STAGE_WORK_DIR}/mnt"
+if [[ "${DISTO}" == "buster" ]]; then
 
-log "Compile kernel for Pi 4"
-pushd ${LINUX_DIR}
+    # Do this to the WORK folder of this stage
+    pushd ${STAGE_WORK_DIR}
+    MNT_DIR="${STAGE_WORK_DIR}/mnt"
 
-log "Copy Kernel config"
-cp "${STAGE_DIR}/FILES/.config-${KERNEL_BRANCH}-v7l" ./.config
+    log "Compile kernel for Pi 4"
+    pushd ${LINUX_DIR}
+
+    log "Copy Kernel config"
+    cp "${STAGE_DIR}/FILES/.config-${KERNEL_BRANCH}-v7l" ./.config
 
 
-make clean
+    make clean
 
-#KERNEL=kernel7l ARCH=arm CROSS_COMPILE=arm-linux-gnueabihf- make bcm2711_defconfig
-yes "" | KERNEL=kernel7l ARCH=arm CROSS_COMPILE=arm-linux-gnueabihf- make -j $J_CORES zImage modules dtbs
+    #KERNEL=kernel7l ARCH=arm CROSS_COMPILE=arm-linux-gnueabihf- make bcm2711_defconfig
+    yes "" | KERNEL=kernel7l ARCH=arm CROSS_COMPILE=arm-linux-gnueabihf- make -j $J_CORES zImage modules dtbs
 
-log "Saving kernel as ${STAGE_WORK_DIR}/kernel7l.img"
-cp arch/arm/boot/zImage "${MNT_DIR}/boot/kernel7l.img"
+    log "Saving kernel as ${STAGE_WORK_DIR}/kernel7l.img"
+    cp arch/arm/boot/zImage "${MNT_DIR}/boot/kernel7l.img"
 
-log "Copy the kernel modules for Pi 4"
-make -j $J_CORES ARCH=arm CROSS_COMPILE=arm-linux-gnueabihf- INSTALL_MOD_PATH="$MNT_DIR" modules_install
+    log "Copy the kernel modules for Pi 4"
+    make -j $J_CORES ARCH=arm CROSS_COMPILE=arm-linux-gnueabihf- INSTALL_MOD_PATH="$MNT_DIR" modules_install
 
-log "Copy the DTBs for Pi 4"
-sudo cp arch/arm/boot/dts/*.dtb "${MNT_DIR}/boot/"
-sudo cp arch/arm/boot/dts/overlays/*.dtb* "${MNT_DIR}/boot/overlays/"
-sudo cp arch/arm/boot/dts/overlays/README "${MNT_DIR}/boot/overlays/"
+    log "Copy the DTBs for Pi 4"
+    sudo cp arch/arm/boot/dts/*.dtb "${MNT_DIR}/boot/"
+    sudo cp arch/arm/boot/dts/overlays/*.dtb* "${MNT_DIR}/boot/overlays/"
+    sudo cp arch/arm/boot/dts/overlays/README "${MNT_DIR}/boot/overlays/"
 
-# out of linux 
-popd
+    # out of linux 
+    popd
 
-#return 
-popd
+    #return 
+    popd
+
+
+fi
