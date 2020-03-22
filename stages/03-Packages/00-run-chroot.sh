8fb45328b4c969c2b97389d7d780bc11c3ed7325
Stephen Oliver <steve@infincia.com>
2020-03-22 14:01:37 -0400

    Reorganize package install and fail build on error
    This merges the multiple install steps into one using groups of
    package lists, preventing the builder from having to run the
    post-processing steps over and over.
    
    We also exit 1 if specific commands fail so that the build doesn't
    continue and break later on due to missing or incorrectly installed
    packages

--- stages/03-Packages/00-run-chroot.sh
+++ stages/03-Packages/00-run-chroot.sh
@@ -18,134 +18,97 @@ apt-mark hold raspberrypi-bootloader
 apt-mark hold raspberrypi-kernel
 
 # Install kernel-headers before apt-get update
-DEBIAN_FRONTEND=noninteractive sudo apt-get -yq install raspberrypi-kernel-headers
+DEBIAN_FRONTEND=noninteractive sudo apt-get -yq install raspberrypi-kernel-headers || exit 1
 
 # Install libraspberrypi-dev before apt-get update
-DEBIAN_FRONTEND=noninteractive sudo apt-get -yq install libraspberrypi-dev libraspberrypi-dev libraspberrypi-bin libraspberrypi0
+DEBIAN_FRONTEND=noninteractive sudo apt-get -yq install libraspberrypi-dev libraspberrypi-dev libraspberrypi-bin libraspberrypi0 firmware-misc-nonfree || exit 1
 apt-mark hold libraspberrypi-dev libraspberrypi-bin libraspberrypi0
 
 # Latest package source
 # sudo rm -rf /var/lib/apt/lists/*
 # sudo apt-get clean
-sudo apt-get update
+sudo apt-get update || exit 1
 
-# Install essentials
-DEBIAN_FRONTEND=noninteractive sudo apt-get -yq install python-pip python-setuptools python-dev python3-dev
-DEBIAN_FRONTEND=noninteractive sudo apt-get -yq install aircrack-ng
 if [[ "${DISTO}" == "stretch" ]]; then
     # on buster the gnuplot package pulls in 670MB of other stuff we don't want, it's a giant waste of space
-    DEBIAN_FRONTEND=noninteractive sudo apt-get -yq install gnuplot
+    GNUPLOT="gnuplot"
 fi
-DEBIAN_FRONTEND=noninteractive sudo apt-get -yq install dnsmasq
-DEBIAN_FRONTEND=noninteractive sudo apt-get -yq install socat
-# DEBIAN_FRONTEND=noninteractive sudo apt-get -yq install --assume-no wireshark-common
-# DEBIAN_FRONTEND=noninteractive sudo apt-get -yq install tshark
-DEBIAN_FRONTEND=noninteractive sudo apt-get -yq install ser2net
-DEBIAN_FRONTEND=noninteractive sudo apt-get -yq install mc
 
-#DEBIAN_FRONTEND=noninteractive sudo apt-get -yq install gtk-doc-tools
+# Build tools
+BUILD_TOOLS="git build-essential autotools-dev automake libtool autoconf"
 
-DEBIAN_FRONTEND=noninteractive sudo apt-get -yq install build-essential autotools-dev automake
-DEBIAN_FRONTEND=noninteractive sudo apt-get -yq install libtool
-DEBIAN_FRONTEND=noninteractive sudo apt-get -yq install autoconf
-DEBIAN_FRONTEND=noninteractive sudo apt-get -yq install libsdl1.2-dev
-DEBIAN_FRONTEND=noninteractive sudo apt-get -yq install libconfig++-dev libreadline-dev
-DEBIAN_FRONTEND=noninteractive sudo apt-get -yq install git libpcap-dev wiringpi iw usbmount
-DEBIAN_FRONTEND=noninteractive sudo apt-get -yq install libjpeg8-dev indent libfreetype6-dev ttf-dejavu-core
-DEBIAN_FRONTEND=noninteractive sudo apt-get -yq install python-m2crypto
-DEBIAN_FRONTEND=noninteractive sudo apt-get -yq install dos2unix
-DEBIAN_FRONTEND=noninteractive sudo apt-get -yq install dosfstools
-DEBIAN_FRONTEND=noninteractive sudo apt-get -yq install hostapd
-DEBIAN_FRONTEND=noninteractive sudo apt-get -yq install pump
-DEBIAN_FRONTEND=noninteractive sudo apt-get -yq install libpng12-dev
-DEBIAN_FRONTEND=noninteractive sudo apt-get -yq install python-future python-attr
-DEBIAN_FRONTEND=noninteractive sudo apt-get -yq install libfontconfig1-dev 
-DEBIAN_FRONTEND=noninteractive sudo apt-get -yq install libsdl2-dev
-DEBIAN_FRONTEND=noninteractive sudo apt-get -yq install libsdl1.2-dev
-DEBIAN_FRONTEND=noninteractive sudo apt-get -yq install libav-tools
-DEBIAN_FRONTEND=noninteractive sudo apt-get -yq install firmware-misc-nonfree
-
-DEBIAN_FRONTEND=noninteractive sudo apt install -y libegl1-mesa libegl1-mesa-dev libgles2-mesa libgles2-mesa-dev libgbm-dev 
-
-# dependencies for OpenHDMicroservice
-DEBIAN_FRONTEND=noninteractive sudo apt-get -yq install build-essential git python libboost-dev libboost-program-options-dev libboost-system-dev libasio-dev
-
-#Arducam Multi Camera Adapter Module V2.1
-DEBIAN_FRONTEND=noninteractive sudo apt-get -yq install  i2c-tools
-
-# v4l2loopback and flir dependencies NEED TO CHECK IF THEY ARE ALREADY INSTALLED
-DEBIAN_FRONTEND=noninteractive sudo apt-get -yq install vim
-DEBIAN_FRONTEND=noninteractive sudo apt-get -yq install fuse
-DEBIAN_FRONTEND=noninteractive sudo apt-get -yq install libusb-1.0-0-dev
-
-# mavlink-router build dependencies
-DEBIAN_FRONTEND=noninteractive sudo apt-get -yq install python3-future
-
-# openhdvid dependency
-DEBIAN_FRONTEND=noninteractive sudo apt-get -yq install python3-picamera
-
-# install omxplayer. Used by SSync to display video
-DEBIAN_FRONTEND=noninteractive sudo apt-get -yq install omxplayer
-
-# libsodium, used by svpcom
-apt-get --yes --force-yes install libsodium-dev || exit 1
-
-# text to speech for QOpenHD
-DEBIAN_FRONTEND=noninteractive sudo apt-get -yq install libspeechd-dev flite1-dev flite speech-dispatcher-flite
-
-# Qt dependencies
-DEBIAN_FRONTEND=noninteractive sudo apt-get install build-essential \
-                                                    libfontconfig1-dev libdbus-1-dev \
-                                                    libfreetype6-dev libicu-dev libinput-dev \
-                                                    libxkbcommon-dev libsqlite3-dev libssl-dev libpng-dev \
-                                                    libjpeg-dev libglib2.0-dev \
-                                                    libasound2-dev pulseaudio libpulse-dev libdouble-conversion-dev \
-                                                    libudev-dev libinput-dev libts-dev mtdev-tools
+# Libraries used by our own code
+LIBRARIES="libpcap-dev libpng-dev libsdl2-dev libsdl1.2-dev libconfig++-dev libreadline-dev 
+           libjpeg8-dev libusb-1.0-0-dev libsodium-dev"
 
 
-# Remove packages that conflict with the workings of EZ-Wifibroadcast
-DEBIAN_FRONTEND=noninteractive sudo apt-get -yq purge wireless-regdb
-# DEBIAN_FRONTEND=noninteractive sudo apt-get -yq purge wpasupplicant
-DEBIAN_FRONTEND=noninteractive sudo apt-get -yq purge crda
-DEBIAN_FRONTEND=noninteractive sudo apt-get -yq purge cron
-
-#Untested Packages for possible removal
-#DEBIAN_FRONTEND=noninteractive sudo apt-get -yq purge dbus
-#DEBIAN_FRONTEND=noninteractive sudo apt-get -yq purge dhcpcd5
-#DEBIAN_FRONTEND=noninteractive sudo apt-get -yq purge isc-dhcp-client
-#DEBIAN_FRONTEND=noninteractive sudo apt-get -yq purge isc-dhcp-common
+# Python interpreters, we won't need python2 much longer
+PYTHON2="python-pip python-dev python-setuptools"
+PYTHON3="python3-pip python3-dev python3-setuptools"
 
 
-# Remove packages for space savings
-DEBIAN_FRONTEND=noninteractive sudo apt-get -yq purge apt-transport-https
-DEBIAN_FRONTEND=noninteractive sudo apt-get -yq purge aptitude
-DEBIAN_FRONTEND=noninteractive sudo apt-get -yq purge aptitude-common
-DEBIAN_FRONTEND=noninteractive sudo apt-get -yq purge apt-listchanges
-DEBIAN_FRONTEND=noninteractive sudo apt-get -yq purge avahi-daemon
-DEBIAN_FRONTEND=noninteractive sudo apt-get -yq purge cifs_utils
-DEBIAN_FRONTEND=noninteractive sudo apt-get -yq purge curl
-DEBIAN_FRONTEND=noninteractive sudo apt-get -yq purge iptables
-DEBIAN_FRONTEND=noninteractive sudo apt-get -yq purge triggerhappy
+# Python dependencies used by our own code
+PYTHON2_DEPENDENCIES="python-future python-attr python-m2crypto python-rpi.gpio"
+PYTHON3_DEPENDENCIES="python3-future python3-attr python3-picamera python3-rpi.gpio"
 
 
-DEBIAN_FRONTEND=noninteractive sudo apt-get -yq install libgstreamer1.0-0
-DEBIAN_FRONTEND=noninteractive sudo apt-get -yq install gstreamer1.0
-DEBIAN_FRONTEND=noninteractive sudo apt-get -yq install gstreamer1.0-plugins-base 
-DEBIAN_FRONTEND=noninteractive sudo apt-get -yq install gstreamer1.0-plugins-good 
-DEBIAN_FRONTEND=noninteractive sudo apt-get -yq install gstreamer1.0-plugins-bad 
-DEBIAN_FRONTEND=noninteractive sudo apt-get -yq install gstreamer1.0-plugins-ugly 
-DEBIAN_FRONTEND=noninteractive sudo apt-get -yq install gstreamer1.0-libav 
-DEBIAN_FRONTEND=noninteractive sudo apt-get -yq install gstreamer1.0-tools 
-DEBIAN_FRONTEND=noninteractive sudo apt-get -yq install gstreamer1.0-alsa 
-DEBIAN_FRONTEND=noninteractive sudo apt-get -yq install gstreamer1.0-pulseaudio
-DEBIAN_FRONTEND=noninteractive sudo apt-get -yq install gstreamer1.0-omx-rpi-config
+# Command line utilities used at runtime by the OpenHD scripts
+SCRIPT_DEPENDENCIES="wiringpi usbmount ser2net i2c-tools fuse socat dos2unix dosfstools libav-tools indent omxplayer"
 
-#Python3 GPIO
-sudo apt-get -y install python3-rpi.gpio
 
-# Clean Up
-DEBIAN_FRONTEND=noninteractive sudo apt-get -yq clean
-DEBIAN_FRONTEND=noninteractive sudo apt-get -yq autoremove
+FONT_SUPPORT="libfontconfig1-dev libfreetype6-dev ttf-dejavu-core"
+
+
+NETWORK_UTILITIES="hostapd iw pump dnsmasq aircrack-ng"
+
+
+DEVELOPMENT_UTILITIES="vim mc"
+
+
+MESA_DRM_STACK="libegl1-mesa libegl1-mesa-dev libgles2-mesa libgles2-mesa-dev libgbm-dev"
+
+
+MICROSERVICE_DEPENDENCIES="libboost-dev libboost-program-options-dev libboost-system-dev libasio-dev"
+
+
+TEXTTOSPEECH_QOPENHD="libspeechd-dev flite1-dev flite speech-dispatcher-flite"
+
+
+QT_DEPENDENCIES="libfontconfig1-dev libdbus-1-dev libfreetype6-dev libicu-dev libinput-dev libxkbcommon-dev
+                 libsqlite3-dev libssl-dev libpng-dev libjpeg8-dev libglib2.0-dev libasound2-dev pulseaudio
+                 libpulse-dev libdouble-conversion-dev libudev-dev libinput-dev libts-dev mtdev-tools"
+
+
+GSTREAMER="libgstreamer1.0-0 gstreamer1.0-plugins-base gstreamer1.0-plugins-good gstreamer1.0-plugins-bad
+           gstreamer1.0-plugins-ugly gstreamer1.0-libav gstreamer1.0-tools gstreamer1.0-alsa gstreamer1.0-pulseaudio
+           gstreamer1.0-omx-rpi-config"
+
+
+PURGE="wireless-regdb crda cron apt-transport-https aptitude aptitude-common apt-listchanges
+       avahi-daemon cifs-utils curl iptables triggerhappy"
+
+
+DEBIAN_FRONTEND=noninteractive sudo apt-get -y --no-install-recommends install \
+${BUILD_TOOLS} \
+${LIBRARIES} \
+${PYTHON2} \
+${PYTHON3} \
+${PYTHON2_DEPENDENCIES} \
+${PYTHON3_DEPENDENCIES} \
+${SCRIPT_DEPENDENCIES} \
+${FONT_SUPPORT} \
+${NETWORK_UTILITIES} \
+${DEVELOPMENT_UTILITIES} \
+${MESA_DRM_STACK} \
+${MICROSERVICE_DEPENDENCIES} \
+${TEXTTOSPEECH_QOPENHD} \
+${QT_DEPENDENCIES} \
+${GSTREAMER} ${GNUPLOT} || exit 1
+
+DEBIAN_FRONTEND=noninteractive sudo apt-get -yq purge ${PURGE} || exit 1
+
+DEBIAN_FRONTEND=noninteractive sudo apt-get -yq clean || exit 1
+DEBIAN_FRONTEND=noninteractive sudo apt-get -yq autoremove || exit 1
 
 if [ ${APT_CACHER_NG_ENABLED} == "true" ]; then
     rm /etc/apt/apt.conf.d/10cache
