8cbcf6124c4119817a55c70d3d8ff0f71eab83cb
Stephen Oliver <steve@infincia.com>
2020-03-22 15:16:43 -0400

    Fail build if kernel can't be copied

--- stages/02-Kernel/03-run.sh
+++ stages/02-Kernel/03-run.sh
@@ -17,15 +17,15 @@ make clean
 yes "" | KERNEL=kernel7 ARCH=arm CROSS_COMPILE=arm-linux-gnueabihf- make -j $J_CORES zImage modules dtbs
 
 log "Saving kernel as ${STAGE_WORK_DIR}/kernel7.img"
-cp arch/arm/boot/zImage "${MNT_DIR}/boot/kernel7.img"
+cp arch/arm/boot/zImage "${MNT_DIR}/boot/kernel7.img" || exit 1
 
 log "Copy the kernel modules for Pi 2, Pi 3, Pi 3+, or Compute Module 3/3+"
 make -j $J_CORES ARCH=arm CROSS_COMPILE=arm-linux-gnueabihf- INSTALL_MOD_PATH="$MNT_DIR" modules_install
 
 log "Copy the DTBs for Pi 2, Pi 3, Pi 3+, or Compute Module 3/3+"
-sudo cp arch/arm/boot/dts/*.dtb "${MNT_DIR}/boot/"
-sudo cp arch/arm/boot/dts/overlays/*.dtb* "${MNT_DIR}/boot/overlays/"
-sudo cp arch/arm/boot/dts/overlays/README "${MNT_DIR}/boot/overlays/"
+sudo cp arch/arm/boot/dts/*.dtb "${MNT_DIR}/boot/" || exit 1
+sudo cp arch/arm/boot/dts/overlays/*.dtb* "${MNT_DIR}/boot/overlays/" || exit 1
+sudo cp arch/arm/boot/dts/overlays/README "${MNT_DIR}/boot/overlays/" || exit 1
 
 # out of linux 
 popd
--- stages/02-Kernel/04-run.sh
+++ stages/02-Kernel/04-run.sh
@@ -19,15 +19,15 @@ if [[ "${DISTO}" == "buster" ]]; then
     yes "" | KERNEL=kernel7l ARCH=arm CROSS_COMPILE=arm-linux-gnueabihf- make -j $J_CORES zImage modules dtbs
 
     log "Saving kernel as ${STAGE_WORK_DIR}/kernel7l.img"
-    cp arch/arm/boot/zImage "${MNT_DIR}/boot/kernel7l.img"
+    cp arch/arm/boot/zImage "${MNT_DIR}/boot/kernel7l.img" || exit 1
 
     log "Copy the kernel modules for Pi 4"
     make -j $J_CORES ARCH=arm CROSS_COMPILE=arm-linux-gnueabihf- INSTALL_MOD_PATH="$MNT_DIR" modules_install
 
     log "Copy the DTBs for Pi 4"
-    sudo cp arch/arm/boot/dts/*.dtb "${MNT_DIR}/boot/"
-    sudo cp arch/arm/boot/dts/overlays/*.dtb* "${MNT_DIR}/boot/overlays/"
-    sudo cp arch/arm/boot/dts/overlays/README "${MNT_DIR}/boot/overlays/"
+    sudo cp arch/arm/boot/dts/*.dtb "${MNT_DIR}/boot/" || exit 1
+    sudo cp arch/arm/boot/dts/overlays/*.dtb* "${MNT_DIR}/boot/overlays/" || exit 1
+    sudo cp arch/arm/boot/dts/overlays/README "${MNT_DIR}/boot/overlays/" || exit 1
 
     # out of linux 
     popd
