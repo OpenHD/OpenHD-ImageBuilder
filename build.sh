476fc413482b12bbfade8e71dfe636343d0b1aed
Stephen Oliver <steve@infincia.com>
2020-03-23 19:40:40 -0400

    Fix indentation/whitespace in main builder scripts

--- build.sh
+++ build.sh
@@ -2,7 +2,7 @@
 # shellcheck disable=SC2119,SC1091
 
 if [ -f config ]; then
-	source config
+    source config
 fi
 
 IMAGE_ARCH=$1
@@ -14,38 +14,38 @@ echo ""
 
 
 if [[ "${IMAGE_ARCH}" == "" || ${DISTRO} == "" ]]; then
-	IMAGE_ARCH="pi"
-	DISTRO="stretch"
+    IMAGE_ARCH="pi"
+    DISTRO="stretch"
 
-	echo "Usage: ./build.sh pi [stretch | buster]"
-	echo ""
-	echo "Options:"
-	echo ""
-	echo "                  pi stretch: standard image, supports Pi Zero, Pi 2, Pi 3, CM3"
-	echo ""
-	echo "                   pi buster: testing image, for Pi Compute Module 3+ and Pi 4"
-	echo ""
-	echo ""
-	echo "------------------------------------------------------"
-	echo ""
+    echo "Usage: ./build.sh pi [stretch | buster]"
+    echo ""
+    echo "Options:"
+    echo ""
+    echo "                  pi stretch: standard image, supports Pi Zero, Pi 2, Pi 3, CM3"
+    echo ""
+    echo "                   pi buster: testing image, for Pi Compute Module 3+ and Pi 4"
+    echo ""
+    echo ""
+    echo "------------------------------------------------------"
+    echo ""
 fi
 
 
 if [[ "$IMAGE_ARCH" == "pi" && "${DISTRO}" == "stretch" ]]; then
-	echo "Building pi stretch image"
+    echo "Building pi stretch image"
 
-	BASE_IMAGE_URL=${PI_STRETCH_BASE_IMAGE_URL}
-	BASE_IMAGE=${PI_STRETCH_BASE_IMAGE}
-	KERNEL_BRANCH=${PI_STRETCH_KERNEL_BRANCH}
+    BASE_IMAGE_URL=${PI_STRETCH_BASE_IMAGE_URL}
+    BASE_IMAGE=${PI_STRETCH_BASE_IMAGE}
+    KERNEL_BRANCH=${PI_STRETCH_KERNEL_BRANCH}
 fi
 
 
 if [[ "$IMAGE_ARCH" == "pi" && "${DISTRO}" == "buster" ]]; then
-	echo "Building pi buster image"
+    echo "Building pi buster image"
 
-	BASE_IMAGE_URL=${PI_BUSTER_BASE_IMAGE_URL}
-	BASE_IMAGE=${PI_BUSTER_BASE_IMAGE}
-	KERNEL_BRANCH=${PI_BUSTER_KERNEL_BRANCH}
+    BASE_IMAGE_URL=${PI_BUSTER_BASE_IMAGE_URL}
+    BASE_IMAGE=${PI_BUSTER_BASE_IMAGE}
+    KERNEL_BRANCH=${PI_BUSTER_KERNEL_BRANCH}
 fi
 
 echo ""
@@ -53,80 +53,82 @@ echo "------------------------------------------------------"
 
 
 run_stage(){
-	log ""
-	log ""
-	log "======================================================"
-	log "Begin ${STAGE_DIR}"
-	STAGE="$(basename "${STAGE_DIR}")"
-	STAGE_WORK_DIR="${WORK_DIR}/${STAGE}"
-	pushd "${STAGE_DIR}" > /dev/null
+    log ""
+    log ""
+    log "======================================================"
+    log "Begin ${STAGE_DIR}"
+    STAGE="$(basename "${STAGE_DIR}")"
+    STAGE_WORK_DIR="${WORK_DIR}/${STAGE}"
+    pushd "${STAGE_DIR}" > /dev/null
 
-	# Create the Work folder
-	mkdir -p "${STAGE_WORK_DIR}"
+    # Create the Work folder
+    mkdir -p "${STAGE_WORK_DIR}"
 
-	# Check wether to skip or not
-	if [ ! -f "${STAGE_DIR}/SKIP" ]; then
-        	# mount the image for this stage
-	        if [ ! -f "${STAGE_DIR}/SKIP_IMAGE" ]; then
-        	    # Copy the image from the previous stage
-	            if [ -f "${PREV_WORK_DIR}/IMAGE.img" ]; then
-			unmount_image
-			cp "${PREV_WORK_DIR}/IMAGE.img" "${STAGE_WORK_DIR}/IMAGE.img"
-			mount_image
-        	    else
-                	log "[ERROR] No image to copy in ${PREV_WORK_DIR}/"
-		        fi
-	        fi
+    # Check wether to skip or not
+    if [ ! -f "${STAGE_DIR}/SKIP" ]; then
+        # mount the image for this stage
+        if [ ! -f "${STAGE_DIR}/SKIP_IMAGE" ]; then
+            # Copy the image from the previous stage
+            if [ -f "${PREV_WORK_DIR}/IMAGE.img" ]; then
+                unmount_image
+                cp "${PREV_WORK_DIR}/IMAGE.img" "${STAGE_WORK_DIR}/IMAGE.img"
+                mount_image
+            else
+                log "[ERROR] No image to copy in ${PREV_WORK_DIR}/"
+            fi
+        fi
 
-        	# iterate different files
-	        for i in {00..99}; do
-        	    if [ -x ${i}-run.sh ]; then
-			SKIP_STEP="${STAGE_DIR}/SKIP_STEP${i}"
-	        	if [ ! -f "${SKIP_STEP}" ]; then
-	                	log "Begin ${STAGE_DIR}/${i}-run.sh"
-        	        	./${i}-run.sh
-                		log "End ${STAGE_DIR}/${i}-run.sh"
-				touch "${SKIP_STEP}"
-				
-			fi
-	            fi
-        	    if [ -f ${i}-run-chroot.sh ]; then
-			SKIP_CH_STEP="${STAGE_DIR}/SKIP_CH_STEP${i}"
-	        	if [ ! -f "${SKIP_CH_STEP}" ]; then
-                		log "Begin ${STAGE_DIR}/${i}-run-chroot.sh"
-	                	on_chroot < ${i}-run-chroot.sh
-	                	log "End ${STAGE_DIR}/${i}-run-chroot.sh"
-				touch "${SKIP_CH_STEP}"
-			fi
-        	    fi
-	        done
-	fi
+        # iterate different files
+        for i in {00..99}; do
 
-	# SKIP this stage next time
-	touch "${STAGE_DIR}/SKIP"
+            if [ -x ${i}-run.sh ]; then
+                SKIP_STEP="${STAGE_DIR}/SKIP_STEP${i}"
+                if [ ! -f "${SKIP_STEP}" ]; then
+                    log "Begin ${STAGE_DIR}/${i}-run.sh"
+                    ./${i}-run.sh
+                    log "End ${STAGE_DIR}/${i}-run.sh"
+                    touch "${SKIP_STEP}"
+                fi
+            fi
 
-    	PREV_STAGE="${STAGE}"
-    	PREV_STAGE_DIR="${STAGE_DIR}"
-	PREV_WORK_DIR="${WORK_DIR}/${STAGE}"
+            if [ -f ${i}-run-chroot.sh ]; then
+                SKIP_CH_STEP="${STAGE_DIR}/SKIP_CH_STEP${i}"
+                if [ ! -f "${SKIP_CH_STEP}" ]; then
+                    log "Begin ${STAGE_DIR}/${i}-run-chroot.sh"
+                    on_chroot < ${i}-run-chroot.sh
+                    log "End ${STAGE_DIR}/${i}-run-chroot.sh"
+                    touch "${SKIP_CH_STEP}"
+                fi
+            fi
 
-	if [ ! -f "${STAGE_DIR}/SKIP_IMAGE" ]; then
-		unmount_image
-	fi
+        done
+    fi
 
-	popd > /dev/null
-	log "End ${STAGE_DIR}"
+    # SKIP this stage next time
+    touch "${STAGE_DIR}/SKIP"
+
+    PREV_STAGE="${STAGE}"
+    PREV_STAGE_DIR="${STAGE_DIR}"
+    PREV_WORK_DIR="${WORK_DIR}/${STAGE}"
+
+    if [ ! -f "${STAGE_DIR}/SKIP_IMAGE" ]; then
+        unmount_image
+    fi
+
+    popd > /dev/null
+    log "End ${STAGE_DIR}"
 }
 
 if [ "$(id -u)" != "0" ]; then
-	echo "Please run as root" 1>&2
-	exit 1
+    echo "Please run as root" 1>&2
+    exit 1
 fi
 
 
 
 if [ -z "${IMG_NAME}" ]; then
-	echo "IMG_NAME not set" 1>&2
-	exit 1
+    echo "IMG_NAME not set" 1>&2
+    exit 1
 fi
 
 # Variables
@@ -234,16 +236,16 @@ log "Begin ${BASE_DIR}"
 # Iterate trough the steps
 find ./stages -name '*.sh' -type f | xargs chmod 775
 for STAGE_DIR in "${BASE_DIR}/stages/"*; do
-	if [ -d "${STAGE_DIR}" ]; then
-		run_stage
-	fi
+    if [ -d "${STAGE_DIR}" ]; then
+        run_stage
+    fi
 done
 
 # rename the image according to the build date, the builder/openhd repo versions
 OPENHD_VERSION=$(cat ${WORK_DIR}/openhd_version.txt)
 if [ -f "${PREV_WORK_DIR}/IMAGE.img" ]; then
-	mkdir -p "${DEPLOY_DIR}" || true
-	cp "${PREV_WORK_DIR}/IMAGE.img" "${DEPLOY_DIR}/${IMG_NAME}-${OPENHD_VERSION}-${DISTRO}.img"
+    mkdir -p "${DEPLOY_DIR}" || true
+    cp "${PREV_WORK_DIR}/IMAGE.img" "${DEPLOY_DIR}/${IMG_NAME}-${OPENHD_VERSION}-${DISTRO}.img"
 fi
 
 #  Clean up SKIP_STEP files since we finished the build
