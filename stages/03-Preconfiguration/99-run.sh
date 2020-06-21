9aa50671631d5f4d214e837d704a16b90eaf4d31
Stephen Oliver <steve@infincia.com>
2020-06-21 15:28:14 -0400

    Copy overlay files in stage 3

--- stages/03-Preconfiguration/99-run.sh
+++ stages/03-Preconfiguration/99-run.sh
@@ -6,6 +6,10 @@ cp ${STAGE_WORK_DIR}/mnt/openhd_version.txt ${WORK_DIR}/openhd_version.txt
 
 MNT_DIR="${STAGE_WORK_DIR}/mnt"
 
+
+cp -r "${STAGE_DIR}/FILES/overlay/." "$MNT_DIR"
+
+
 # Rename the DOS partition
 BOOT_MNT_DIR="${STAGE_WORK_DIR}/mnt/boot"
 BOOT_LOOP_DEV="$(findmnt -nr -o source $BOOT_MNT_DIR)"
