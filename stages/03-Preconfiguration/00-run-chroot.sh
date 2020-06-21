f45e779ec523f4b2445453376e377b4fe2ee0399
Stephen Oliver <steve@infincia.com>
2020-06-20 22:46:27 -0400

    Merge the chroot steps in stage 3

--- stages/03-Preconfiguration/00-run-chroot.sh
+++ stages/03-Preconfiguration/00-run-chroot.sh
@@ -17,3 +17,92 @@ if [[ "${DISTRO}" == "stretch" ]]; then
     ln -sf /opt/vc/lib/pkgconfig/brcmglesv2.pc /opt/vc/lib/pkgconfig/glesv2.pc
     ln -sf /opt/vc/lib/pkgconfig/brcmvg.pc     /opt/vc/lib/pkgconfig/vg.pc
 fi
+
+
+
+#Ensure the runlevel is multi-target (3) could possibly be lower...
+sudo systemctl set-default multi-user.target
+
+
+rm /etc/init.d/dnsmasq
+rm /etc/init.d/dhcpcd
+
+
+#disable unneeded services
+sudo systemctl disable dhcpcd.service
+sudo systemctl disable dnsmasq.service
+sudo systemctl disable regenerate_ssh_host_keys.service
+sudo systemctl disable cron.service
+sudo systemctl disable syslog.service
+sudo systemctl disable journald.service
+sudo systemctl disable triggerhappy.service
+sudo systemctl disable avahi-daemon.service
+sudo systemctl disable ser2net.service
+sudo systemctl disable systemd-timesyncd.service
+sudo systemctl disable hciuart.service
+sudo systemctl disable anacron.service
+sudo systemctl disable syslog.service
+sudo systemctl disable triggerhappy.service
+sudo systemctl disable ser2net.service
+sudo systemctl disable systemd-timesyncd.service
+sudo systemctl disable hciuart.service
+sudo systemctl disable exim4.service
+sudo systemctl mask hostapd.service
+sudo systemctl mask wpa_supplicant.service
+
+#Disable does not work on PLYMOUTH
+sudo systemctl mask plymouth-start.service
+sudo systemctl mask plymouth-read-write.service
+sudo systemctl mask plymouth-quit-wait.service
+sudo systemctl mask plymouth-quit.service
+sudo systemctl disable systemd-journal-flush.service
+#this service updates runlevel changes. Set desired runlevel prior to this being disabled
+sudo systemctl disable systemd-update-utmp.service
+sudo systemctl disable networking.service
+
+#Mask difficult to disable services
+systemctl stop systemd-journald.service
+systemctl disable systemd-journald.service
+systemctl mask systemd-journald.service
+
+
+
+#enable /dev/video0
+#sudo modprobe bcm2835-v4l2
+
+
+
+
+sudo rm /etc/init.d/resize2fs_once
+
+# Disable ZeroTier service
+#sudo systemctl disable zerotier-one
+
+# Copy tty autologin stuff
+systemctl enable getty@tty1.service
+systemctl enable getty@tty2.service
+systemctl enable getty@tty3.service
+systemctl enable getty@tty4.service
+systemctl enable getty@tty5.service
+systemctl enable getty@tty6.service
+systemctl enable getty@tty7.service
+systemctl enable getty@tty8.service
+systemctl enable getty@tty9.service
+systemctl enable getty@tty10.service
+systemctl enable getty@tty11.service
+systemctl enable getty@tty12.service
+
+#disable sync option for usbmount
+sudo sed -i 's/sync,//g' /etc/usbmount/usbmount.conf
+
+#change hostname
+CURRENT_HOSTNAME=`sudo cat /etc/hostname | sudo tr -d " \t\n\r"`
+NEW_HOSTNAME="openhd"
+if [ $? -eq 0 ]; then
+  sudo sh -c "echo '$NEW_HOSTNAME' > /etc/hostname"
+  sudo sed -i "s/127.0.1.1.*$CURRENT_HOSTNAME/127.0.1.1\t$NEW_HOSTNAME/g" /etc/hosts
+fi
+
+apt -y autoremove
+apt -y clean
+
