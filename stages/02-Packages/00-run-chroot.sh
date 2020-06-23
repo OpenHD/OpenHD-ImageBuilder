5d15027ae311cc87c2c04c6dcd1c174b935ba0b4
Stephen Oliver <steve@infincia.com>
2020-06-22 22:12:17 -0400

    Update repo location for 2.0 repo
    The repos for 2.0 and 2.1 are separate to ensure that 2.0 users are
    only receiving bugfixes and not new stuff that could affect their
    existing setup.
    
    Upgrades from 2.0 to 2.1 will be done by the microservice code, it
    will replace the file in /etc/apt/sources.list.d/ and then trigger
    update && upgrade of the openhd package.

--- stages/02-Packages/00-run-chroot.sh
+++ stages/02-Packages/00-run-chroot.sh
@@ -20,11 +20,12 @@ if [ "${IMAGE_ARCH}" == "pi" ]; then
 fi
 
 apt-get install -y apt-transport-https
-curl -1sLf 'https://dl.cloudsmith.io/public/openhd/openhd/cfg/gpg/gpg.B9F0E99CF5787237.key' | apt-key add -
+curl -1sLf 'https://dl.cloudsmith.io/public/openhd/openhd-2-0/cfg/gpg/gpg.B9F0E99CF5787237.key' | apt-key add -
 
 # this gets removed after packages are installed to ensure that openhd 2.0 images dont't change once written to a card,
 # in-place upgrades will be in openhd 2.1 and require more testing before we enable it
-echo "deb https://dl.cloudsmith.io/public/openhd/openhd/deb/${OS} ${DISTRO} main" > /etc/apt/sources.list.d/openhd-2-0.list
+
+echo "deb https://dl.cloudsmith.io/public/openhd/openhd-2-0/deb/${OS} ${DISTRO} main" > /etc/apt/sources.list.d/openhd-2-0.list
 
 apt purge raspberrypi-bootloader raspberrypi-kernel
 
