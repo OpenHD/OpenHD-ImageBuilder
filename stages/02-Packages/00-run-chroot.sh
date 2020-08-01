45c7159c20cfc177ccce9145ff97ad6cc51b5af3
Stephen Oliver <steve@infincia.com>
2020-07-31 21:50:18 -0400

    Install specific version of openhd-linux-pi

--- stages/02-Packages/00-run-chroot.sh
+++ stages/02-Packages/00-run-chroot.sh
@@ -37,7 +37,7 @@ apt purge raspberrypi-kernel firmware-atheros
 
 apt-get update || exit 1
 
-OPENHD_PACKAGES="openhd=2.0.0rc11 openhd-linux-pi"
+OPENHD_PACKAGES="openhd=2.0.0rc11 openhd-linux-pi=20200731.1"
 
 # Python interpreters, we won't need python2 much longer
 PYTHON2="python-pip python-dev python-setuptools"
