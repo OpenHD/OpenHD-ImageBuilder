f4d7a800c8617e362cb9a8b4a84a2431ecd00003
Stephen Oliver <steve@infincia.com>
2020-11-01 00:59:17 -0400

    Update pi kernel, remove separate pi zero package

--- stages/02-Packages/00-run-chroot.sh
+++ stages/02-Packages/00-run-chroot.sh
@@ -38,7 +38,7 @@ apt purge raspberrypi-kernel firmware-atheros
 
 apt-get update || exit 1
 
-OPENHD_PACKAGES="openhd=2.0.2 openhd-linux-pi=20200826.1 openhd-linux-pizero=20200829.1"
+OPENHD_PACKAGES="openhd=2.0.2 openhd-linux-pi=20201031.2"
 
 # Python interpreters, we won't need python2 much longer
 PYTHON2="python-pip python-dev python-setuptools"
