8f5392589b62a3b47f5bec61051f3f1968263b8f
Stephen Oliver <steve@infincia.com>
2020-12-07 17:09:58 -0500

    Don't automatically log in anymore
    The system is no longer based on ttys and shell scripts but systemd
    units, so we don't need to override the tty behavior anymore

--- stages/03-Preconfiguration/00-run-chroot.sh
+++ stages/03-Preconfiguration/00-run-chroot.sh
@@ -72,20 +72,6 @@ sudo rm /etc/init.d/resize2fs_once
 # Disable ZeroTier service
 #sudo systemctl disable zerotier-one
 
-# Copy tty autologin stuff
-systemctl enable getty@tty1.service
-systemctl enable getty@tty2.service
-systemctl enable getty@tty3.service
-systemctl enable getty@tty4.service
-systemctl enable getty@tty5.service
-systemctl enable getty@tty6.service
-systemctl enable getty@tty7.service
-systemctl enable getty@tty8.service
-systemctl enable getty@tty9.service
-systemctl enable getty@tty10.service
-systemctl enable getty@tty11.service
-systemctl enable getty@tty12.service
-
 #change hostname
 CURRENT_HOSTNAME=`sudo cat /etc/hostname | sudo tr -d " \t\n\r"`
 NEW_HOSTNAME="openhd"
