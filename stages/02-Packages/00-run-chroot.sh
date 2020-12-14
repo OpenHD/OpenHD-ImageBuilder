b2eea60589e4aa2d367d58b8ef7a7494f4efab3d
Stephen Oliver <steve@infincia.com>
2020-12-13 22:42:55 -0500

    2.0.7

--- stages/02-Packages/00-run-chroot.sh
+++ stages/02-Packages/00-run-chroot.sh
@@ -38,7 +38,7 @@ apt purge raspberrypi-kernel firmware-atheros
 
 apt-get update || exit 1
 
-OPENHD_PACKAGES="openhd=2.0.6"
+OPENHD_PACKAGES="openhd=2.0.7"
 
 # Python interpreters, we won't need python2 much longer
 PYTHON2="python-pip python-dev python-setuptools"
