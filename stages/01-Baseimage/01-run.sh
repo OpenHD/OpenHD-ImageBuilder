ac4a1202d40d717166242d0473a5b63ae63296eb
Stephen Oliver <steve@infincia.com>
2021-03-11 17:22:52 -0500

    Add some more space on the pi image, it's tiny

--- stages/01-Baseimage/01-run.sh
+++ stages/01-Baseimage/01-run.sh
@@ -3,11 +3,11 @@ pushd ${STAGE_WORK_DIR}
 
 if [[ "${OS}" == "raspbian" ]]; then
 
-    log "Create 2Gb empty image"
-    dd if=/dev/zero of=temp.img bs=1 count=1 seek=2G
+    log "Create 4Gb empty image"
+    dd if=/dev/zero of=temp.img bs=1 count=1 seek=4G
 
 
-    log "Enlarge the downloaded image by 2Gb"
+    log "Enlarge the downloaded image by 4Gb"
     cat temp.img >> IMAGE.img
 
     log "fdisk magic to enlarge the main partition"
