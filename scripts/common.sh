0adaab2aab3fcd117823de2f8fbea2f1176d4527
pilotnbr1 <pilotnbr1@gmail.com>
2021-08-29 13:50:42 -0400

    Mount conf for jetson

--- scripts/common.sh
+++ scripts/common.sh
@@ -12,23 +12,29 @@ mount_image () {
 
     if [[ "${HAVE_BOOT_PART}" == "true" ]]; then
         log "Mounting boot partition: ${BOOT_PART}"
+        echo "Mounting boot partition: ${BOOT_PART}"
 
         BOOT_OFFSET=$(echo "$PARTED_OUT" | grep -e "^ ${BOOT_PART}" | xargs echo -n | cut -d" " -f 2 | tr -d B)
         BOOT_LENGTH=$(echo "$PARTED_OUT" | grep -e "^ ${BOOT_PART}" | xargs echo -n | cut -d" " -f 4 | tr -d B)
         BOOT_DEV=$(losetup --show -f -o "${BOOT_OFFSET}" --sizelimit "${BOOT_LENGTH}" "${IMG_FILE}")
         log "/boot: offset $BOOT_OFFSET, length $BOOT_LENGTH"
+        echo "/boot: offset $BOOT_OFFSET, length $BOOT_LENGTH"
     fi
 
     if [[ "${HAVE_CONF_PART}" == "true" ]]; then
         log "Mounting conf partition: ${CONF_PART}"
+        echo "Mounting conf partition: ${CONF_PART}"
 
-        CONF_OFFSET=$(echo "$PARTED_OUT" | grep -e "^ ${CONF_PART}" | xargs echo -n | cut -d" " -f 2 | tr -d B)
-        CONF_LENGTH=$(echo "$PARTED_OUT" | grep -e "^ ${CONF_PART}" | xargs echo -n | cut -d" " -f 4 | tr -d B)
+        #for 2 digit grep matches there is no space here - "^${CONF_PART}"... 
+        CONF_OFFSET=$(echo "$PARTED_OUT" | grep -e "^${CONF_PART}" | xargs echo -n | cut -d" " -f 2 | tr -d B)
+        CONF_LENGTH=$(echo "$PARTED_OUT" | grep -e "^${CONF_PART}" | xargs echo -n | cut -d" " -f 4 | tr -d B)
         CONF_DEV=$(losetup --show -f -o "${CONF_OFFSET}" --sizelimit "${CONF_LENGTH}" "${IMG_FILE}")
         log "/conf: offset $CONF_OFFSET, length $CONF_LENGTH"
+        echo "/conf: offset $CONF_OFFSET, length $CONF_LENGTH"
     fi
 
     log "Mounting root partition: ${ROOT_PART}"
+    echo "Mounting root partition: ${ROOT_PART}"
 
     ROOT_OFFSET=$(echo "$PARTED_OUT" | grep -e "^ ${ROOT_PART}" | xargs echo -n | cut -d" " -f 2 | tr -d B)
     ROOT_LENGTH=$(echo "$PARTED_OUT" | grep -e "^ ${ROOT_PART}" | xargs echo -n | cut -d" " -f 4 | tr -d B)
@@ -36,6 +42,7 @@ mount_image () {
 
 
     log "/:     offset $ROOT_OFFSET, length $ROOT_LENGTH"
+    echo "/:     offset $ROOT_OFFSET, length $ROOT_LENGTH"
 
     # Make sure the mount point is there
     MNT_DIR="${STAGE_WORK_DIR}/mnt"
@@ -48,6 +55,7 @@ mount_image () {
     sleep 5
 
     log "Resizing root to fill partition"
+    echo "Resizing root to fill partition"
 
     # Resize to full size
     LOOP_DEV="$(findmnt -nr -o source $MNT_DIR)"
@@ -56,21 +64,30 @@ mount_image () {
 
     # disable dir_index due to a weird bug when running qemu in a 32-bit chroot on 64-bit x86 hardware, readdir() fails in strange ways
     if [[ "${BIT}" == "32" ]]; then
-        log "Disabling dir_index on ${LOOP_DEV}"
+        echo "Disabling dir_index on ${LOOP_DEV}"
         tune2fs -O ^dir_index ${LOOP_DEV} || true
     fi
 
     if [[ "${HAVE_BOOT_PART}" == "true" ]]; then
-        # mount the BOOT partition
+        echo "mount the BOOT partition"
         mountpoint -q "${MNT_DIR}/boot" || mount "$IMG_FILE" -o loop,offset=${BOOT_OFFSET},rw,sizelimit=${BOOT_LENGTH} "${MNT_DIR}/boot"
     fi
 
     if [[ "${HAVE_CONF_PART}" == "true" ]]; then
-        # mount the conf partition
+        echo "mount the conf partition"
+        
+        if [ -d "$MNT_DIR/conf" ]; then
+            echo "conf DIR exists already..."
+        else
+            mkdir $MNT_DIR/conf
+            echo "Created conf DIR..."
+        fi
+        
         mountpoint -q "${MNT_DIR}/conf" || mount "$IMG_FILE" -o loop,offset=${CONF_OFFSET},rw,sizelimit=${CONF_LENGTH} "${MNT_DIR}/conf"
     fi
 
     log "Finished mounting"
+    echo "Finished mounting"
 }
 export -f mount_image
 
