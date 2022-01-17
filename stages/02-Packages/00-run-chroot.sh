62789fa78a63683da1a259d7cfcc17e1eb39dd88
MarioFPV <57532232+MarioFPVdev@users.noreply.github.com>
2022-01-17 22:55:59 +0000

    Update 00-run-chroot.sh
    Compile and install from sources the rtl8812au drivers for Jetson Nano

--- stages/02-Packages/00-run-chroot.sh
+++ stages/02-Packages/00-run-chroot.sh
@@ -104,14 +104,20 @@ PURGE="wireless-regdb crda cron avahi-daemon cifs-utils curl iptables triggerhap
 
 echo "install openhd version-${OPENHD_PACKAGE}"
 if [[ "${OS}" == "ubuntu" ]]; then
-    echo "install some Jetson essential apps and rtl8812au driver from sources"
-    sudo apt install -y git nano python-pip jtop build-essential libelf-dev
+    echo "Install some Jetson essential libraries and compile rtl8812au driver from sources"
+    sudo apt install -y git nano python-pip build-essential libelf-dev
     pip install -U jetson-stats
-    sudo apt-get install linux-headers-`uname -r`
-    sudo cd ../../ && git clone https://github.com/svpcom/rtl8812au.git
-    cd rtl* && make && make install
-    cp -r /rtl8812au/88XXau_wfb.ko /lib/modules/4.9.253-tegra/kernel/drivers/net/wireless/realtek/rtl8812au/
-    mv /lib/modules/4.9.253-tegra/kernel/drivers/net/wireless/realtek/rtl8812au/rtl8812au.ko /lib/modules/4.9.253-tegra/kernel/drivers/net/wireless/realtek/rtl8812au/rtl8812au.ko.bak
+    sudo apt-get install linux-headers-4.15.0-166
+    sudo apt-get install linux-headers-4.18.0.25-generic
+    git clone https://github.com/svpcom/rtl8812au.git
+    cd rtl8812au
+    sudo sed -i 's/CONFIG_PLATFORM_I386_PC = y/CONFIG_PLATFORM_I386_PC = n/' Makefile
+    sudo sed -i 's/CONFIG_PLATFORM_ARM_NV_NANO = n/CONFIG_PLATFORM_ARM_NV_NANO = y/' Makefile
+    make KVER=4.9.253-tegra all && make install KVER=4.9.253-tegra all
+    cp -r 88XXau_wfb.ko /lib/modules/4.9.253-tegra/kernel/drivers/net/wireless/realtek/rtl8812au/
+    cd ..
+    cd /lib/modules/4.9.253-tegra/kernel/drivers/net/wireless/realtek/rtl8812au/
+    mv rtl8812au.ko rtl8812au.ko.bak
 fi
 
 DEBIAN_FRONTEND=noninteractive apt-get -y --no-install-recommends install \
