74bb20e4b4682af4ebf587180e0e3c9896e6be28
MarioFPV <57532232+MarioFPVdev@users.noreply.github.com>
2022-01-28 18:06:47 +0000

    Skip removing apps to test the Jetson 4GB. Installing the patched drivers instead of compiling them.

--- stages/02-Packages/00-run-chroot.sh
+++ stages/02-Packages/00-run-chroot.sh
@@ -49,15 +49,15 @@ if [[ "${OS}" == "ubuntu" ]]; then
     sudo cat /etc/apt/sources.list.d/nvidia-l4t-apt-source.list
 
     #remove some nvidia packages... if building from nvidia base image 
-	#gdm isn't used, remove lightdm instead, removed new line in #60
-    sudo apt remove ubuntu-desktop
-    sudo apt remove libreoffice-writer chromium-browser chromium* yelp unity thunderbird rhythmbox nautilus gnome-software
-    sudo apt remove ubuntu-artwork ubuntu-sounds ubuntu-wallpapers ubuntu-wallpapers-bionic
-    sudo apt remove vlc-data lightdm
-    sudo apt remove unity-settings-daemon packagekit wamerican mysql-common libgdm1
-    sudo apt remove ubuntu-release-upgrader-gtk ubuntu-web-launchers
-    sudo apt remove --purge libreoffice* gnome-applet* gnome-bluetooth gnome-desktop* gnome-sessio* gnome-user* gnome-shell-common gnome-control-center gnome-screenshot
-    sudo apt autoremove
+    #gdm isn't used, remove lightdm instead, removed new line in #60
+    #sudo apt remove ubuntu-desktop
+    #sudo apt remove libreoffice-writer chromium-browser chromium* yelp unity thunderbird rhythmbox nautilus gnome-software
+    #sudo apt remove ubuntu-artwork ubuntu-sounds ubuntu-wallpapers ubuntu-wallpapers-bionic
+    #sudo apt remove vlc-data lightdm
+    #sudo apt remove unity-settings-daemon packagekit wamerican mysql-common libgdm1
+    #sudo apt remove ubuntu-release-upgrader-gtk ubuntu-web-launchers
+    #sudo apt remove --purge libreoffice* gnome-applet* gnome-bluetooth gnome-desktop* gnome-sessio* gnome-user* gnome-shell-common gnome-control-center gnome-screenshot
+    #sudo apt autoremove
     
 fi
 
@@ -82,10 +82,6 @@ apt install -y apt-transport-https curl
 curl -1sLf 'https://dl.cloudsmith.io/public/openhd/openhd-2-1/cfg/gpg/gpg.0AD501344F75A993.key' | apt-key add -
 curl -1sLf 'https://dl.cloudsmith.io/public/openhd/openhd-2-1-testing/cfg/gpg/gpg.58A6C96C088A96BF.key' | apt-key add -
 sudo apt-get install -y apt-utils
-curl -1sLf 'https://dl.cloudsmith.io/public/openhd/openhd-2-1-testing/setup.deb.sh' | sudo -E bash && \
-curl -1sLf 'https://dl.cloudsmith.io/public/openhd/openhd-2-1/setup.deb.sh' | sudo -E bash && \
-apt update
-
 
 echo "deb https://dl.cloudsmith.io/public/openhd/openhd-2-1/deb/${OS} ${DISTRO} main" > /etc/apt/sources.list.d/openhd-2-1.list
 
@@ -109,19 +105,13 @@ export DEBIAN_FRONTEND=noninteractive
 echo "install openhd version-${OPENHD_PACKAGE}"
 if [[ "${OS}" == "ubuntu" ]]; then
     echo "Install some Jetson essential libraries and compile rtl8812au driver from sources"
-    sudo apt install -y git nano python-pip build-essential libelf-dev
+    sudo apt install -y git nano python-pip build-essential libelf-dev rtl8812au=20220125-1
     sudo -H pip install -U jetson-stats
-    sudo apt-get install linux-headers-4.15.0-166
-    sudo apt-get install linux-headers-4.18.0.25-generic
-    git clone https://github.com/svpcom/rtl8812au.git
-    cd rtl8812au
-    sudo sed -i 's/CONFIG_PLATFORM_I386_PC = y/CONFIG_PLATFORM_I386_PC = n/' Makefile
-    sudo sed -i 's/CONFIG_PLATFORM_ARM_NV_NANO = n/CONFIG_PLATFORM_ARM_NV_NANO = y/' Makefile
-    make KVER=4.9.253-tegra all && make install KVER=4.9.253-tegra all
-    cp -r 88XXau_wfb.ko /lib/modules/4.9.253-tegra/kernel/drivers/net/wireless/realtek/rtl8812au/
-    cd ..
+    cd /lib/modules/4.9.253/kernel/drivers/net/wireless
+    cp -r 88XXau.ko /lib/modules/4.9.253-tegra/kernel/drivers/net/wireless/realtek/rtl8812au/
     cd /lib/modules/4.9.253-tegra/kernel/drivers/net/wireless/realtek/rtl8812au/
     mv rtl8812au.ko rtl8812au.ko.bak
+    mv 88XXau.ko rtl8812au.ko
     echo '#!/bin/bash' >> /usr/local/bin/video.sh && printf "\nsudo nvpmodel -m 0 | sudo jetson_clocks\nsudo iw wlan0 set freq 5320\nsudo iw wlan0 set txpower fixed 3100\necho \"nameserver 1.1.1.1\" > /etc/resolv.conf" >> /usr/local/bin/video.sh
     printf "[Unit]\nDescription=\"Jetson Nano clocks\"\nAfter=openhdinterface.service\n[Service]\nExecStart=/usr/local/bin/video.sh\n[Install]\nWantedBy=multi-user.target\nAlias=video.service" >> /etc/systemd/system/video.service
     sudo chmod u+x /usr/local/bin/video.sh
