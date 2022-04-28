5e687cda906185e0242f2b89d01e28bf5f95d995
Raphael Scholle <68374617+raphaelscholle@users.noreply.github.com>
2022-04-28 18:53:56 +0200

    update packages

--- install_dep.sh
+++ install_dep.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-apt-get -y install unzip bzip2 curl git qemu qemu-user-static binfmt-support build-essential
+apt -y install unzip bzip2 p7zip-full curl git qemu qemu-user-static binfmt-support build-essential
