35cedf9c5bed2f21c338457d8e611fcd032eedc2
Raphael Scholle <raphael.scholle@uni-wuppertal.de>
2022-05-02 18:07:59 +0200

    include kernel (jetson)

--- stages/02-Packages/00-run-chroot.sh
+++ stages/02-Packages/00-run-chroot.sh
@@ -59,6 +59,8 @@ if [[ "${OS}" == "ubuntu" ]]; then
     sudo apt remove --purge libreoffice* gnome-applet* gnome-bluetooth gnome-desktop* gnome-sessio* gnome-user* gnome-shell-common gnome-control-center gnome-screenshot
     sudo apt autoremove
     
+    PLATFORM_PACKAGES="${PLATFORM_PACKAGES} ${KERNEL_PACKAGE}"
+
 fi
 
 
