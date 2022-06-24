07f0bd3288c037055be792b88ff17b865b9f361c
Raphael Scholle <68374617+raphaelscholle@users.noreply.github.com>
2022-06-24 14:20:31 +0200

    enable gstreamer-qt5 jetson

--- stages/02-Packages/00-run-chroot.sh
+++ stages/02-Packages/00-run-chroot.sh
@@ -57,7 +57,7 @@ if [[ "${OS}" == "ubuntu" ]]; then
     sudo apt remove --purge libreoffice* gnome-applet* gnome-bluetooth gnome-desktop* gnome-sessio* gnome-user* gnome-shell-common gnome-control-center gnome-screenshot
     sudo apt autoremove
     
-    PLATFORM_PACKAGES="openhd-linux-jetson openhd-qt-jetson-nano-bionic qopenhd"
+    PLATFORM_PACKAGES="openhd-linux-jetson gstreamer1.0-qt5 openhd-qt-jetson-nano-bionic qopenhd"
 
 fi
 
