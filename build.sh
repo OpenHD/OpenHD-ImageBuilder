805e46f3bb86302e6479702a4cfec796647986ae
Raphael Scholle <68374617+raphaelscholle@users.noreply.github.com>
2022-09-14 00:03:49 +0200

    syntax #2

--- build.sh
+++ build.sh
@@ -205,8 +205,9 @@ if [ -f "${PREV_WORK_DIR}/IMAGE.img" ]; then
     rm ${PREV_WORK_DIR}/IMAGE.img
     cp ${PREV_WORK_DIR}/image.zip ${IMAGE_PATH_NAME}
     rm -Rf ${PREV_WORK_DIR}
-    unzip ${IMAGE_PATH_NAME}*.zip
-    rm ${IMAGE_PATH_NAME}*.zip
+    cd ${IMAGE_PATH_NAME}
+    unzip *.zip
+    rm -Rf *.zip
     df -h
 fi
 
