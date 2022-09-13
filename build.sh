1a969e2b625b3e06d8873a27b224c1863c4c0da6
Raphael Scholle <68374617+raphaelscholle@users.noreply.github.com>
2022-09-13 22:42:20 +0200

    typo

--- build.sh
+++ build.sh
@@ -201,7 +201,7 @@ if [ -f "${PREV_WORK_DIR}/IMAGE.img" ]; then
     df -h
     mkdir -p "${DEPLOY_DIR}" || true
     ls -l ${PREV_WORK_DIR}/IMAGE.img
-    zip ${PREV_WORK_DIR}image.zip ${PREV_WORK_DIR}/IMAGE.img
+    zip ${PREV_WORK_DIR}/image.zip ${PREV_WORK_DIR}/IMAGE.img
     rm ${PREV_WORK_DIR}/IMAGE.img
     cp "${PREV_WORK_DIR}/*.zip" ${IMAGE_PATH_NAME}
     rm -Rf ${PREV_WORK_DIR}
