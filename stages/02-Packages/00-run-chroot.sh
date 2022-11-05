fb83a7709cf89328c4b250039119eb33ac8750aa
Raphael Scholle <68374617+raphaelscholle@users.noreply.github.com>
2022-11-05 15:59:54 +0100

    X86

--- stages/02-Packages/00-run-chroot.sh
+++ stages/02-Packages/00-run-chroot.sh
@@ -112,8 +112,7 @@ if [[ "${TESTING}" == "testing" ]] ; then
       sudo apt install -y xinit net-tools libxcb-xinerama0 libxcb-util1 libgstreamer-plugins-base1.0-dev
       #sudo apt install -y dkms nvidia-driver-510 nvidia-dkms-510
       sudo apt install -y network-manager libspdlog-dev network-manager-gnome openhd-linux-x86
-      cd /opt/X86/
-      git clone https://github.com/OpenHD/rtl8812au-5.2.20 
+      git clone https://github.com/OpenHD/rtl8812au-5.2.20 /opt/X86/
       cat /proc/version
       fi
 
