53e7117547bba50628d3b069ae858ce719a671c7
Raphael Scholle <68374617+raphaelscholle@users.noreply.github.com>
2022-11-18 17:33:38 +0100

    attempt to fix jetson 2gb

--- stages/03-Preconfiguration/00-run-chroot.sh
+++ stages/03-Preconfiguration/00-run-chroot.sh
@@ -113,7 +113,7 @@ if [[ "${OS}" == "ubuntu" ]]; then
        echo "debug ubuntu jetson"
        touch /boot/openhd/jetson.txt
        touch /boot/openhd/air.txt
-       rm /usr/sbin/nv-oem-config-firstboot*
+       systemctl disable nv-oem-config.service
 fi
 
 if [[ "${OS}" == "ubuntu-x86" ]] ; then
