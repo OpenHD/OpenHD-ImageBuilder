5df53435ec0fd090903c311813b83cbab969ca0d
Raphael Scholle <68374617+raphaelscholle@users.noreply.github.com>
2022-11-30 19:05:42 +0100

    make shortcuts on x86 desktop

--- stages/03-Preconfiguration/00-run-chroot.sh
+++ stages/03-Preconfiguration/00-run-chroot.sh
@@ -122,6 +122,9 @@ if [[ "${OS}" == "ubuntu" ]]; then
 fi
 
 if [[ "${OS}" == "ubuntu-x86" ]] ; then
+       cp /opt/Overlay/desktop-truster.sh /etc/profile.d/desktop-truster.sh
+       #this script needs to be executable by every user
+       chmod +777 /etc/profile.d/desktop-truster.sh
        mkdir -p /boot/openhd/
        touch /boot/openhd/x86.txt
        touch /boot/openhd/ground.txt
@@ -129,16 +132,17 @@ if [[ "${OS}" == "ubuntu-x86" ]] ; then
        systemctl disable qopenhd
        git clone https://github.com/OpenHD/OpenHD-ImageBuilder
        cd OpenHD-ImageBuilder
-       gio set shortcuts/OpenHD-Air.desktop metadata::trusted true
-       gio set shortcuts/OpenHD-Ground.desktop metadata::trusted true
-       gio set shortcuts/QOpenHD.desktop metadata::trusted true
        chmod a+x  shortcuts/OpenHD-Air.desktop
 	   chmod a+x  shortcuts/OpenHD-Ground.desktop
 	   chmod a+x  shortcuts/QOpenHD.desktop
 	   sudo cp shortcuts/* /usr/share/applications/
-       sudo cp shortcuts/* /home/openhd/Desktop/
+       sudo cp shortcuts/*.desktop /home/openhd/Desktop/
 	   sudo cp shortcuts/OpenHD.ico /opt/
        sudo nmcli connection delete Onair
+       gio set /home/openhd/Desktop/OpenHD-Air.desktop metadata::trusted true
+       gio set /home/openhd/Desktop/OpenHD-Ground.desktop metadata::trusted true
+       gio set /home/openhd/Desktop/QOpenHD.desktop metadata::trusted true
+
        #cp -rf initial-setup.sh /opt/X86/
        #cp -rf initial-setup.service /etc/systemd/system/
        #sudo chmod +x /opt/X86/initial-setup.sh
