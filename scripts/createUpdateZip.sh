aa1e080466ed725ce9dc31abf0ad3584aeb0297d
Raphael Scholle <68374617+raphaelscholle@users.noreply.github.com>
2023-02-03 17:25:43 +0100

    create zip-update-script

---
+++ scripts/createUpdateZip.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+# The folder to store the .deb files
+UPDATE_FOLDER="/boot/update"
+
+# The name of the zip file
+ZIP_FILE="$UPDATE_FOLDER/update.zip"
+
+# Create the update folder, if it doesn't exist
+if [ ! -d "$UPDATE_FOLDER" ]; then
+  mkdir "$UPDATE_FOLDER"
+fi
+
+# Remove all existing .deb files in the update folder
+rm "$UPDATE_FOLDER"/*.deb
+
+# Update the package lists
+apt update
+
+# Download the .deb files for packages that need to be updated
+for package in $(apt list --upgradable | awk '/\*/ {print $1}'); do
+  apt download "$package" -o APT::Sandbox::User=root --yes
+  mv *.deb "$UPDATE_FOLDER"
+done
+
+# Create the zip file
+zip -r "$ZIP_FILE" "$UPDATE_FOLDER"
+
+This script updates the package lists and downloads the .deb files for packages that need to be updated. The downloaded .deb files are stored in the /boot/update folder, and the folder is zipped into a file named update.zip.
