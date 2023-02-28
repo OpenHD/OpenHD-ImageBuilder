17a18f2321fbc24e589bcf4cd96b9e6eaf8e66a2
Raphael Scholle <68374617+raphaelscholle@users.noreply.github.com>
2023-02-28 18:35:47 +0100

    add debian

--- stages/01-Baseimage/01-run.sh
+++ stages/01-Baseimage/01-run.sh
@@ -40,6 +40,45 @@ w
 EOF
 fi
 
+if [[ "${OS}" == "debian" ]] ; then
+
+    #Makes the images flashable with raspberry pi imager
+    log "Calculate difference between original Image and Wanted size (7GB)" 
+    WANTEDSIZE="14168000000"
+    FILESIZE=$(stat -c%s "IMAGE.img")
+    DIFFERENCE=$(expr $WANTEDSIZE - $FILESIZE)
+    DIFFERENCE=$(expr $DIFFERENCE - 1)
+
+
+    log "Create empty image" #this will be attached to the base image to increase the size of it
+    dd if=/dev/zero of=temp.img bs=1 count=1 seek=$DIFFERENCE
+
+
+    log "Enlarge the downloaded image"
+    cat temp.img >> IMAGE.img
+
+    log "fdisk magic to enlarge the main partition"
+    #calculating image offsets
+    PARTED_OUT=$(parted -s IMAGE.img unit s print)
+    ROOT_OFFSET=$(echo "$PARTED_OUT" | grep -e "^ ${ROOT_PART}"| xargs echo -n \
+        | cut -d" " -f 2 | tr -d s)
+    
+    echo "ROOT OFFSET: $ROOT_OFFSET"
+    echo "IF EDITING THIS SCRIPT THE SPACES MATER FOR FDISK COMMANDS"
+    #Now we delete Partition 2, write a new partition 2 and write the calculated size to have a larger root-partition)
+
+    fdisk IMAGE.img <<EOF
+d
+2
+n
+p
+2
+${ROOT_OFFSET}
+
+w
+EOF
+fi
+
 #jetson images now use custom base images because of a change to the jetson-jetpack 
 # if [[ "${OS}" == "ubuntu" ]]; then
 #      log "Calculate difference between original Image and Wanted size (~16GB)"
