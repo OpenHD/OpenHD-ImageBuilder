b450a09ee0f6781852ff7417a463948e76cd6bf5
Raphael Scholle <68374617+raphaelscholle@users.noreply.github.com>
2023-03-05 04:55:31 +0100

    curl doesn't need to be installed twice ;)

--- stages/02-Packages/00-run-chroot.sh
+++ stages/02-Packages/00-run-chroot.sh
@@ -10,7 +10,7 @@ set -e
 function install_raspbian_packages {
     PLATFORM_PACKAGES_HOLD="firmware-atheros raspberrypi-kernel libraspberrypi-dev libraspberrypi-bin libraspberrypi0 libraspberrypi-doc"
     PLATFORM_PACKAGES_REMOVE="nfs-common libcamera* raspberrypi-kernel"
-    PLATFORM_PACKAGES="apt-transport-https git curl apt-utils firmware-atheros firmware-misc-nonfree open-hd-web-ui openhd-userland openhd-linux-pi openhd-linux-pi-headers libsdl2-dev libspdlog-dev libcamera-openhd libcamera-apps-openhd libavcodec-dev libavformat-dev mavsdk gstreamer1.0-gl gst-latest openhd-qt openhd qopenhd libsodium-dev libpcap-dev git nano openssh-server libboost-filesystem1.74-dev meson"
+    PLATFORM_PACKAGES="apt-transport-https git apt-utils firmware-atheros firmware-misc-nonfree open-hd-web-ui openhd-userland openhd-linux-pi openhd-linux-pi-headers libsdl2-dev libspdlog-dev libcamera-openhd libcamera-apps-openhd libavcodec-dev libavformat-dev mavsdk gstreamer1.0-gl gst-latest openhd-qt openhd qopenhd libsodium-dev libpcap-dev git nano openssh-server libboost-filesystem1.74-dev meson"
 }
 # Debian-Rockship-specific code
 function fix_radxa_apt {
@@ -18,12 +18,12 @@ function fix_radxa_apt {
     apt update
 }
 function install_debian_packages {
-    PLATFORM_PACKAGES="apt-transport-https git curl apt-utils openhd qopenhd rtl8812au-autocompiler procps git cmake dkms"
+    PLATFORM_PACKAGES="apt-transport-https git apt-utils openhd qopenhd rtl8812au-autocompiler procps git cmake dkms"
 }
 # Ubuntu-x86-specific code
 function install_ubuntu_x86_packages {
     PLATFORM_PACKAGES_HOLD="linux-image-5.15.0-57-generic hold grub-efi-amd64-signed linux-generic linux-headers-generic linux-image-generic linux-generic-hwe-22.04 linux-image-generic-hwe-22.04 linux-headers-generic-hwe-22.04"
-    PLATFORM_PACKAGES="apt-transport-https git curl apt-utils git openhdimagewriter nano python3-pip htop libavcodec-dev libavformat-dev libelf-dev libboost-filesystem-dev openhd libspdlog-dev openhd-qt-x86-jammy qopenhd build-essential libfontconfig1-dev libdbus-1-dev libfreetype6-dev libicu-dev libinput-dev libxkbcommon-dev libsqlite3-dev libssl-dev libpng-dev libjpeg-dev libglib2.0-dev libgles2-mesa-dev libgbm-dev libdrm-dev libwayland-dev pulseaudio libpulse-dev flex bison gperf libre2-dev libnss3-dev libdrm-dev libxml2-dev libxslt1-dev libminizip-dev libjsoncpp-dev liblcms2-dev libevent-dev libprotobuf-dev protobuf-compiler '^libxcb.*-dev' libx11-xcb-dev libglu1-mesa-dev libxrender-dev libxi-dev libxkbcommon-x11-dev libgtk2.0-dev libgtk-3-dev libfuse2 mono-complete mono-runtime libmono-system-windows-forms4.0-cil libmono-system-core4.0-cil libmono-system-management4.0-cil libmono-system-xml-linq4.0-cil libgstreamer1.0-dev libgstreamer-plugins-base1.0-dev gstreamer1.0-plugins-base gstreamer1.0-plugins-good gstreamer1.0-plugins-ugly gstreamer1.0-plugins-bad libgstreamer-plugins-bad1.0-dev gstreamer1.0-pulseaudio gstreamer1.0-tools gstreamer1.0-alsa gstreamer1.0-qt5"
+    PLATFORM_PACKAGES="apt-transport-https git apt-utils git openhdimagewriter nano python3-pip htop libavcodec-dev libavformat-dev libelf-dev libboost-filesystem-dev openhd libspdlog-dev openhd-qt-x86-jammy qopenhd build-essential libfontconfig1-dev libdbus-1-dev libfreetype6-dev libicu-dev libinput-dev libxkbcommon-dev libsqlite3-dev libssl-dev libpng-dev libjpeg-dev libglib2.0-dev libgles2-mesa-dev libgbm-dev libdrm-dev libwayland-dev pulseaudio libpulse-dev flex bison gperf libre2-dev libnss3-dev libdrm-dev libxml2-dev libxslt1-dev libminizip-dev libjsoncpp-dev liblcms2-dev libevent-dev libprotobuf-dev protobuf-compiler '^libxcb.*-dev' libx11-xcb-dev libglu1-mesa-dev libxrender-dev libxi-dev libxkbcommon-x11-dev libgtk2.0-dev libgtk-3-dev libfuse2 mono-complete mono-runtime libmono-system-windows-forms4.0-cil libmono-system-core4.0-cil libmono-system-management4.0-cil libmono-system-xml-linq4.0-cil libgstreamer1.0-dev libgstreamer-plugins-base1.0-dev gstreamer1.0-plugins-base gstreamer1.0-plugins-good gstreamer1.0-plugins-ugly gstreamer1.0-plugins-bad libgstreamer-plugins-bad1.0-dev gstreamer1.0-pulseaudio gstreamer1.0-tools gstreamer1.0-alsa gstreamer1.0-qt5"
 }
 
 # Ubuntu-Jetson-specific code
@@ -38,7 +38,7 @@ function fix_jetson_apt {
 }
 function install_jetson_packages {
     PLATFORM_PACKAGES_REMOVE="libreoffice* gnome-applet* gnome-bluetooth gnome-desktop* gnome-sessio* gnome-user* gnome-shell-common gnome-control-center gnome-screenshot ubuntu-release-upgrader-gtk ubuntu-web-launchers unity-settings-daemon packagekit wamerican mysql-common libgdm1 vlc-data lightdm ubuntu-artwork ubuntu-sounds ubuntu-wallpapers ubuntu-wallpapers-bionic ubuntu-desktop gdm3 gnome-* libreoffice-writer chromium-browser chromium* yelp unity thunderbird rhythmbox nautilus gnome-software"
-    PLATFORM_PACKAGES="apt-transport-https git curl apt-utils nano mingetty libgstreamer-plugins-base1.0-dev python-pip libelf-dev libboost1.74-dev openhd-linux-jetson openhd"
+    PLATFORM_PACKAGES="apt-transport-https git apt-utils nano mingetty libgstreamer-plugins-base1.0-dev python-pip libelf-dev libboost1.74-dev openhd-linux-jetson openhd"
 }
 
 function clone_github_repos {
