42d0abc7cf3a1f9b66eb698b539d129878872798
Raphael Scholle <68374617+raphaelscholle@users.noreply.github.com>
2023-03-06 02:59:46 +0100

    hold kernel rock5

--- stages/02-Packages/00-run-chroot.sh
+++ stages/02-Packages/00-run-chroot.sh
@@ -18,6 +18,7 @@ function fix_radxa_apt {
     apt update
 }
 function install_debian_packages {
+    PLATFORM_PACKAGES_HOLD="linux-image-5.10.66-27-rockchip linux-5.10-rock-5-latest"
     PLATFORM_PACKAGES="apt-transport-https git apt-utils openhd qopenhd rtl8812au-autocompiler procps git cmake dkms"
 }
 # Ubuntu-x86-specific code
