bf426c4bfd5bc64e69d2cc8541b4d5a301cd8542
Raphael Scholle <68374617+raphaelscholle@users.noreply.github.com>
2023-03-15 02:17:29 +0100

    Update 01-run.sh

--- stages/01-Baseimage/01-run.sh
+++ stages/01-Baseimage/01-run.sh
@@ -4,8 +4,8 @@ pushd ${STAGE_WORK_DIR}
 function resize_partitions {
 
     #Makes the images flashable with raspberry pi imager
-    log "We now define the size to be ~16GB (the maximum size we have in our github builder, this doesn't affect the output image because we're resizeing it in the end before uploading the image)" 
-    WANTEDSIZE="15872000000"
+    log "We now define the size to be ~15GB (the maximum size we have in our github builder, this doesn't affect the output image because we're resizeing it in the end before uploading the image)" 
+    WANTEDSIZE="14168000000"
     FILESIZE=$(stat -c%s "IMAGE.img")
     DIFFERENCE=$(expr $WANTEDSIZE - $FILESIZE)
     DIFFERENCE=$(expr $DIFFERENCE - 1)
@@ -29,7 +29,7 @@ function resize_partitions {
     echo "IF EDITING THIS SCRIPT THE SPACES MATER FOR FDISK COMMANDS"
     #Now we delete the root Partition , write a new partition and write the calculated size to have a larger root-partition)
 
-fdisk IMAGE.img <<END_CMD
+fdisk IMAGE.img <<EOF
 d
 ${ROOT_PART}
 n
@@ -37,7 +37,7 @@ p
 ${ROOT_PART}
 ${ROOT_OFFSET}
 w
-END_CMD
+EOF
 }
 
 
