f28fa6eb6106ab311e6ad5b0ac734b4b9e6b2a58
Raphael Scholle <68374617+raphaelscholle@users.noreply.github.com>
2023-03-15 03:18:46 +0100

    refactor and add r2 cleaning solution

--- stages/03-Preconfiguration/00-run-chroot.sh
+++ stages/03-Preconfiguration/00-run-chroot.sh
@@ -24,16 +24,15 @@ cd /opt/additionalFiles
 cp motd /etc/motd
 
  if [[ "${OS}" == "debian" ]] ; then
- touch /boot/openhd/rock5.txt
- mv /usr/sbin/login /usr/sbin/nologin
- rm -Rf /lib/modules/5.10.66-27-rockchip-gea60d388902d/kernel/drivers/net/wireless/realtek
- mv /etc/motd /etc/motd2
- cp /opt/additionalFiles/motd2 /etc/motd
- mkdir -p /boot/openhd/
- mkdir -p /etc/systemd/system/getty@tty1.service.d
- touch /boot/openhd/rock5.txt
- touch /boot/openhd/ground.txt
-
+    touch /boot/openhd/rock5.txt
+    mv /usr/sbin/login /usr/sbin/nologin
+    rm -Rf /lib/modules/5.10.66-27-rockchip-gea60d388902d/kernel/drivers/net/wireless/realtek
+    mv /etc/motd /etc/motd2
+    cp /opt/additionalFiles/motd2 /etc/motd
+    mkdir -p /boot/openhd/
+    mkdir -p /etc/systemd/system/getty@tty1.service.d
+    touch /boot/openhd/rock5.txt
+    touch /boot/openhd/ground.txt
  fi
 
  if [[ "${OS}" == "raspbian" ]] ; then
@@ -49,3 +48,82 @@ cp motd /etc/motd
      rm -Rf /boot/dt-blob.bin
      wget https://openhdfpv.org/wp-content/Downloader/dt-blob.bin -P /boot/
  fi
+
+ if [[ "${OS}" == "ubuntu" ]]; then
+       mkdir -p /boot/openhd/
+       mkdir -p /etc/systemd/system/getty@tty1.service.d
+       touch /boot/openhd/jetson.txt
+       touch /boot/openhd/air.txt
+       cp /opt/additionalFiles/override.conf /etc/systemd/system/getty@tty1.service.d/
+fi
+
+if [[ "${OS}" == "ubuntu-x86" ]] ; then
+       sudo usermod -a -G dialout openhd
+       sudo apt remove modemmanager
+       cp /opt/additionalFiles/desktop-truster.sh /etc/profile.d/desktop-truster.sh
+       #this script needs to be executable by every user
+       chmod +777 /etc/profile.d/desktop-truster.sh
+       mkdir -p /boot/openhd/
+       git clone https://github.com/OpenHD/OpenHD-ImageBuilder
+       cd OpenHD-ImageBuilder
+       chmod a+x  shortcuts/OpenHD-Air.desktop
+       chmod a+x  shortcuts/OpenHD-Ground.desktop
+       chmod a+x  shortcuts/QOpenHD.desktop
+       chmod a+x  shortcuts/INAV.desktop
+       chmod a+x  shortcuts/MissionPlanner.desktop
+       chmod a+x  shortcuts/qgroundcontrol.desktop
+       chmod a+x  shortcuts/OpenHD-ImageWriter.desktop
+       sudo cp shortcuts/* /usr/share/applications/
+       sudo cp shortcuts/*.desktop /home/openhd/Desktop/
+       sudo cp shortcuts/*.ico /opt/
+       gio set /home/openhd/Desktop/OpenHD-Air.desktop metadata::trusted true
+       gio set /home/openhd/Desktop/OpenHD-Ground.desktop metadata::trusted true
+       gio set /home/openhd/Desktop/QOpenHD.desktop metadata::trusted true
+       gio set /home/openhd/Desktop/INAV.desktop metadata::trusted true
+       gio set /home/openhd/Desktop/MissionPlanner.desktop metadata::trusted true
+       gio set /home/openhd/Desktop/qgroundcontrol.desktop metadata::trusted true
+       echo "openhd ALL=(ALL:ALL) NOPASSWD: ALL" | sudo tee /etc/sudoers.d/openhd
+       cd /opt
+       mkdir MissionPlanner
+       cd MissionPlanner
+       wget https://firmware.ardupilot.org/Tools/MissionPlanner/MissionPlanner-latest.zip
+       unzip MissionPlanner-latest.zip
+       rm MissionPlanner-latest.zip
+       cd /opt
+       wget https://github.com/iNavFlight/inav-configurator/releases/download/6.0.0-FP2/INAV-Configurator_linux64_6.0.0-FP2.tar.gz
+       tar -zxvf INAV-Configurator_linux64_6.0.0-FP2.tar.gz
+       rm INAV-Configurator_linux64_6.0.0-FP2.tar.gz
+       mv INAV\ Configurator/ INAV
+       cd INAV
+       chmod +x inav-configurator
+       chmod +x chrome_crashpad_handler
+       cd /opt
+       mkdir QGC
+       cd QGC
+       wget https://github.com/mavlink/qgroundcontrol/releases/download/v4.2.4/QGroundControl.AppImage
+       chmod a+x QGroundControl.AppImage
+       chown openhd:openhd QGroundControl.AppImage
+fi
+
+#Install Update-Service
+cp /opt/additionalFiles/update.service /etc/systemd/system/
+cp /opt/additionalFiles/updateOpenHD.sh /usr/local/bin/
+chmod +x /usr/local/bin/updateOpenHD.sh
+systemctl enable update.service
+
+#change hostname to openhd
+CURRENT_HOSTNAME=`sudo cat /etc/hostname | sudo tr -d " \t\n\r"`
+NEW_HOSTNAME="openhd"
+if [ $? -eq 0 ]; then
+  sudo sh -c "echo '$NEW_HOSTNAME' > /etc/hostname"
+  sudo sed -i "s/127.0.1.1.*$CURRENT_HOSTNAME/127.0.1.1\t$NEW_HOSTNAME/g" /etc/hosts
+fi
+
+if [[ "${HAVE_CONF_PART}" == "false" ]] && [[ "${HAVE_BOOT_PART}" == "true" ]]; then
+    # the system expects things to be in /conf now, but on some platforms we use the boot
+    # partition instead of making another one, we may change this in the future
+    ln -s /boot /conf
+fi
+
+apt -y autoremove
+apt -y clean
--- stages/03-Preconfiguration/01-run-chroot.sh
+++ stages/03-Preconfiguration/01-run-chroot.sh
@@ -27,13 +27,6 @@ sudo systemctl disable exim4.service
 sudo systemctl mask hostapd.service
 sudo systemctl enable ssh #we have ssh constantly enabled
 
-#Install Update-Service
-cp /opt/additionalFiles/update.service /etc/systemd/system/
-cp /opt/additionalFiles/updateOpenHD.sh /usr/local/bin/
-chmod +x /usr/local/bin/updateOpenHD.sh
-systemctl enable update.service
-
-
 #Disable does not work on PLYMOUTH
 sudo systemctl mask plymouth-start.service
 sudo systemctl mask plymouth-read-write.service
@@ -42,15 +35,9 @@ sudo systemctl mask plymouth-quit.service
 if [[ "${OS}" != "ubuntu" ]] || [[ "${OS}" != "ubuntu-x86" ]]; then
     echo "OS is NOT ubuntu..disabling journald flush"
     sudo systemctl disable systemd-journal-flush.service
-
 fi
 
 if [[ "${OS}" == "ubuntu" ]]; then
-       mkdir -p /boot/openhd/
-       mkdir -p /etc/systemd/system/getty@tty1.service.d
-       touch /boot/openhd/jetson.txt
-       touch /boot/openhd/air.txt
-       cp /opt/additionalFiles/override.conf /etc/systemd/system/getty@tty1.service.d/
        systemctl disable nv-oem-config-gui.service
        systemctl enable getty@tty1.service
 fi
@@ -59,70 +46,4 @@ if [[ "${OS}" == "debian" ]]; then
        systemctl disable lightdm
 fi
 
-if [[ "${OS}" == "ubuntu-x86" ]] ; then
-       sudo usermod -a -G dialout openhd
-       sudo apt remove modemmanager
-       cp /opt/additionalFiles/desktop-truster.sh /etc/profile.d/desktop-truster.sh
-       #this script needs to be executable by every user
-       chmod +777 /etc/profile.d/desktop-truster.sh
-       mkdir -p /boot/openhd/
-       git clone https://github.com/OpenHD/OpenHD-ImageBuilder
-       cd OpenHD-ImageBuilder
-       chmod a+x  shortcuts/OpenHD-Air.desktop
-       chmod a+x  shortcuts/OpenHD-Ground.desktop
-       chmod a+x  shortcuts/QOpenHD.desktop
-       chmod a+x  shortcuts/INAV.desktop
-       chmod a+x  shortcuts/MissionPlanner.desktop
-       chmod a+x  shortcuts/qgroundcontrol.desktop
-       chmod a+x  shortcuts/OpenHD-ImageWriter.desktop
-       sudo cp shortcuts/* /usr/share/applications/
-       sudo cp shortcuts/*.desktop /home/openhd/Desktop/
-       sudo cp shortcuts/*.ico /opt/
-       gio set /home/openhd/Desktop/OpenHD-Air.desktop metadata::trusted true
-       gio set /home/openhd/Desktop/OpenHD-Ground.desktop metadata::trusted true
-       gio set /home/openhd/Desktop/QOpenHD.desktop metadata::trusted true
-       gio set /home/openhd/Desktop/INAV.desktop metadata::trusted true
-       gio set /home/openhd/Desktop/MissionPlanner.desktop metadata::trusted true
-       gio set /home/openhd/Desktop/qgroundcontrol.desktop metadata::trusted true
-       echo "openhd ALL=(ALL:ALL) NOPASSWD: ALL" | sudo tee /etc/sudoers.d/openhd
-       cd /opt
-       mkdir MissionPlanner
-       cd MissionPlanner
-       wget https://firmware.ardupilot.org/Tools/MissionPlanner/MissionPlanner-latest.zip
-       unzip MissionPlanner-latest.zip
-       rm MissionPlanner-latest.zip
-       cd /opt
-       wget https://github.com/iNavFlight/inav-configurator/releases/download/6.0.0-FP2/INAV-Configurator_linux64_6.0.0-FP2.tar.gz
-       tar -zxvf INAV-Configurator_linux64_6.0.0-FP2.tar.gz
-       rm INAV-Configurator_linux64_6.0.0-FP2.tar.gz
-       mv INAV\ Configurator/ INAV
-       cd INAV
-       chmod +x inav-configurator
-       chmod +x chrome_crashpad_handler
-       cd /opt
-       mkdir QGC
-       cd QGC
-       wget https://github.com/mavlink/qgroundcontrol/releases/download/v4.2.4/QGroundControl.AppImage
-       chmod a+x QGroundControl.AppImage
-       chown openhd:openhd QGroundControl.AppImage
-
-
-fi
-
-#change hostname to openhd
-CURRENT_HOSTNAME=`sudo cat /etc/hostname | sudo tr -d " \t\n\r"`
-NEW_HOSTNAME="openhd"
-if [ $? -eq 0 ]; then
-  sudo sh -c "echo '$NEW_HOSTNAME' > /etc/hostname"
-  sudo sed -i "s/127.0.1.1.*$CURRENT_HOSTNAME/127.0.1.1\t$NEW_HOSTNAME/g" /etc/hosts
-fi
-
-if [[ "${HAVE_CONF_PART}" == "false" ]] && [[ "${HAVE_BOOT_PART}" == "true" ]]; then
-    # the system expects things to be in /conf now, but on some platforms we use the boot
-    # partition instead of making another one, we may change this in the future
-    ln -s /boot /conf
-fi
-
-apt -y autoremove
-apt -y clean
 
