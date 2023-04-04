cc6453c6ae155223df9a32ed8f4674a830daba38
Raphael Scholle <68374617+raphaelscholle@users.noreply.github.com>
2023-04-04 21:05:39 +0200

    Update 01-run.sh

--- stages/01-Baseimage/01-run.sh
+++ stages/01-Baseimage/01-run.sh
@@ -1,12 +1,11 @@
 # Extend Image Size
 pushd ${STAGE_WORK_DIR}
 
-#     #Makes the images flashable with raspberry pi imager
-#     log "We now define the size to be ~15GB (the maximum size we have in our github builder, this doesn't affect the output image because we're resizeing it in the end before uploading the image)" 
-#     WANTEDSIZE="16500000000"
-#     FILESIZE=$(stat -c%s "IMAGE.img")
-#     DIFFERENCE=$(expr $WANTEDSIZE - $FILESIZE)
-#     DIFFERENCE=$(expr $DIFFERENCE - 1)
+    log "We now define the size to be ~15GB (the maximum size we have in our github builder, this doesn't affect the output image because we're resizeing it in the end before uploading the image)" 
+    WANTEDSIZE="16500000000"
+    FILESIZE=$(stat -c%s "IMAGE.img")
+    DIFFERENCE=$(expr $WANTEDSIZE - $FILESIZE)
+    DIFFERENCE=$(expr $DIFFERENCE - 1)
 
 
 #     log "Create empty image" #this will be attached to the base image to increase the size of it
@@ -16,21 +15,21 @@ pushd ${STAGE_WORK_DIR}
 #     log "Enlarge the downloaded image"
 #     cat temp.img >> IMAGE.img
 
-#     log "fdisk magic to enlarge the main partition"
-#     #calculating image offsets
-#     #debug showing all offsets:
-#     echo "Partition information for /dev/sda:"
-#     sudo parted -m IMAGE.img unit s print | awk -F: '$1 ~ /^[0-9]+$/ {print "Partition " $1 ": " $2 " - " $3 " (offset: " $2 "s)"}'
+     log "fdisk magic to enlarge the main partition"
+     #calculating image offsets
+     #debug showing all offsets:
+     echo "Partition information for /dev/sda:"
+     sudo parted -m IMAGE.img unit s print | awk -F: '$1 ~ /^[0-9]+$/ {print "Partition " $1 ": " $2 " - " $3 " (offset: " $2 "s)"}'
 
-#     PARTED_OUT=$(parted -s IMAGE.img unit s print)
-#     ROOT_OFFSET=$(echo "$PARTED_OUT" | grep -e "^ ${ROOT_PART}"| xargs echo -n \
-#         | cut -d" " -f 2 | tr -d s)
+     PARTED_OUT=$(parted -s IMAGE.img unit s print)
+     ROOT_OFFSET=$(echo "$PARTED_OUT" | grep -e "^ ${ROOT_PART}"| xargs echo -n \
+         | cut -d" " -f 2 | tr -d s)
     
-#     echo "ROOT PART: ${ROOT_PART}"
-#     echo "ROOT OFFSET: $ROOT_OFFSET"
-#     echo "IF EDITING THIS SCRIPT THE SPACES MATER FOR FDISK COMMANDS"
-#     #Now we delete the root Partition , write a new partition and write the calculated size to have a larger root-partition)
-#     #DO NOT TOUCH OR REFORMAT .. this is quite annoying
+     echo "ROOT PART: ${ROOT_PART}"
+     echo "ROOT OFFSET: $ROOT_OFFSET"
+     echo "IF EDITING THIS SCRIPT THE SPACES MATER FOR FDISK COMMANDS"
+     #Now we delete the root Partition , write a new partition and write the calculated size to have a larger root-partition)
+     #DO NOT TOUCH OR REFORMAT .. this is quite annoying
 
 #     fdisk IMAGE.img <<EOF
 # d
