2fe5505dafa44eb18393fa6a1baf14701c906d78
Raphael Scholle <68374617+raphaelscholle@users.noreply.github.com>
2023-04-04 23:21:38 +0200

    Update 00-run-chroot.sh

--- stages/02-Packages/00-run-chroot.sh
+++ stages/02-Packages/00-run-chroot.sh
@@ -24,7 +24,7 @@ function fix_radxa_apt {
 function install_radxa-ubuntu_packages {
     PLATFORM_PACKAGES_HOLD=""
     PLATFORM_PACKAGES="procps"
-    ls -a /
+    ls -a /boot/openhd
     cat /conf/config.txt
     cat /conf/before.txt
 }
