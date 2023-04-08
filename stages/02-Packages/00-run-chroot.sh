4c2124cf6e45e7a8f8d0ce960586d9d0eb2d776b
Raphael Scholle <68374617+raphaelscholle@users.noreply.github.com>
2023-04-09 00:10:05 +0200

    Update 00-run-chroot.sh

--- stages/02-Packages/00-run-chroot.sh
+++ stages/02-Packages/00-run-chroot.sh
@@ -22,7 +22,7 @@ function fix_radxa_apt {
     apt update
 }
 function install_radxa-ubuntu_packages {
-    PLATFORM_PACKAGES_HOLD=""
+    PLATFORM_PACKAGES_HOLD="linux-image-5.10.110-1-rockchip linux-headers-5.10.110-1-rockchip"
     PLATFORM_PACKAGES="qopenhd"
     PLATFORM_PACKAGES="procps"
 }
