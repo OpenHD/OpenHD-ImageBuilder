7b986aabe2e41975f7bcc520de20c3f92857d772
Raphael <68374617+raphaelscholle@users.noreply.github.com>
2023-06-15 00:37:50 +0200

    Update 00-run-chroot.sh

--- stages/02-Packages/00-run-chroot.sh
+++ stages/02-Packages/00-run-chroot.sh
@@ -18,8 +18,7 @@ function install_raspbian_packages {
 }
 # Ubuntu-Rockship-specific code
 function install_radxa-ubuntu_packages {
-    apt list --installed
-    PLATFORM_PACKAGES_HOLD="u-boot-rock-5b linux-image-5.10.110-1-rockchip linux-headers-5.10.110-1-rockchip initramfs-tools"
+    PLATFORM_PACKAGES_HOLD="u-boot-rock-5b linux-image-5.10.110-8-rockchip linux-headers-5.10.110-8-rockchip initramfs-tools"
     PLATFORM_PACKAGES="rsync camera-engine-rkaiq gstreamer1.0-rockchip1 qopenhd rtl8812au-autocompiler fan-control-rock5b procps"
 }
 function install_radxa-debian_packages {
