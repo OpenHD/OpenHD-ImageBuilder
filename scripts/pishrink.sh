a75190e359da7faea71d3bab0d63d26439d29caf
Raphael <68374617+raphaelscholle@users.noreply.github.com>
2023-08-14 06:40:51 +0200

    let's remove that function

--- scripts/pishrink.sh
+++ scripts/pishrink.sh
@@ -330,17 +330,17 @@ if [[ $currentsize -eq $minsize ]]; then
   exit 11
 fi
 
-#Add some free space to the end of the filesystem
-//extra_space=$(($currentsize - $minsize))
-logVariables $LINENO extra_space
-echo extra_space
-for space in 5000 1000 100; do
-  if [[ $extra_space -gt $space ]]; then
-    minsize=$(($minsize + $space))
-    break
-  fi
-done
-logVariables $LINENO minsize
+# #Add some free space to the end of the filesystem
+# extra_space=$(($currentsize - $minsize))
+# logVariables $LINENO extra_space
+# echo extra_space
+# for space in 5000 1000 100; do
+#   if [[ $extra_space -gt $space ]]; then
+#     minsize=$(($minsize + $space))
+#     break
+#   fi
+# done
+# logVariables $LINENO minsize
 
 #Shrink filesystem
 info "Shrinking filesystem"
