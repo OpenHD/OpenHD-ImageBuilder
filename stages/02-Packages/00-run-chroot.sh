5cc7fc336de36f0d4ed008c40371803ae485cafc
Raphael <68374617+raphaelscholle@users.noreply.github.com>
2023-10-08 23:51:36 +0200

    Update 00-run-chroot.sh

--- stages/02-Packages/00-run-chroot.sh
+++ stages/02-Packages/00-run-chroot.sh
@@ -10,6 +10,7 @@ sudo apt clean
 sudo apt update && sudo apt upgrade -y
 sudo apt full-upgrade -y
 sudo sed -i 's/buster/bullseye/g' /etc/apt/sources.list
+sudo sed -i '/http:\/\/mirrors.ustc.edu.cn\/debian-security/d' /etc/apt/sources.list
 sudo apt update
 DEBIAN_FRONTEND=noninteractive apt-get install keyboard-configurationsudo
 sudo apt upgrade -y
