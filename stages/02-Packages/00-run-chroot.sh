2cbd0c4212be2b6a544b5b8fe7bbe2e339048a57
Raphael <68374617+raphaelscholle@users.noreply.github.com>
2023-10-13 12:55:54 +0200

    remove graphic drivers

--- stages/02-Packages/00-run-chroot.sh
+++ stages/02-Packages/00-run-chroot.sh
@@ -11,6 +11,8 @@ function prepare_x20 {
     apt install -y ncdu
     rm -Rf /etc/apt/sources.list.d/*
     rm -Rf /etc/apt/sources.list
+    cd /usr/lib/arm-linux-gnueabihf/dri
+    rm -Rf kms_swrast_dri.so armada-drm_dri.so panfrost_dri.so st7735r_dri.so etnaviv_dri.so lima_dri.so pl111_dri.so stm_dri.so exynos_dri.so mcde_dri.so r200_dri.so hx8357d_dri.so ili9225_dri.so r300_dri.so r600_dri.so radeon_dri.so radeonsi_dri.so v3d_dri.so imx-dcss_dri.so imx-drm_dri.so msm_dri.so tegra_dri.so repaper_dri.so virtio_gpu_dri.so ingenic-drm_dri.so nouveau_dri.so nouveau_vieux_dri.so rockchip_dri.so zink_dri.so kgsl_dri.so st7586_dri.so
     touch /etc/apt/sources.list
     apt update
     sed -i '17,35d' /etc/rc.local
