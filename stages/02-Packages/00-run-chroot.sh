d5ac0a163bafff40944f074693a4c763c8e0581a
Raphael <68374617+raphaelscholle@users.noreply.github.com>
2023-11-02 00:58:11 +0100

    Update 00-run-chroot.sh

--- stages/02-Packages/00-run-chroot.sh
+++ stages/02-Packages/00-run-chroot.sh
@@ -8,15 +8,15 @@ set -e
 
 # X20 specific code
 function prepare_x20 {
-    ldd /usr/lib/arm-linux-gnueabihf/libLLVM-11.so.1
     sudo apt update
+    uninstall_oh_my_zsh
     sudo apt install deborphan -y
     sudo apt remove --purge $(deborphan --guess-all) -y
-    sudo apt remove --purge deborphan -y
+    sudo apt remove --purge deborphan vim -y
     sudo apt autoremove -y
-    echo "____________________________"
     rm -Rf /etc/apt/sources.list.d/*
     rm -Rf /etc/apt/sources.list
+    rm -Rf /home/openhd/vencoderDemo
     cd /usr/lib/arm-linux-gnueabihf/dri
     rm -Rf kms_swrast_dri.so mediatek_dri.so armada-drm_dri.so mxsfb-drm_dri.so panfrost_dri.so st7735r_dri.so etnaviv_dri.so lima_dri.so pl111_dri.so stm_dri.so exynos_dri.so mcde_dri.so r200_dri.so hx8357d_dri.so ili9225_dri.so r300_dri.so r600_dri.so radeon_dri.so radeonsi_dri.so v3d_dri.so imx-dcss_dri.so imx-drm_dri.so msm_dri.so tegra_dri.so repaper_dri.so virtio_gpu_dri.so ingenic-drm_dri.so nouveau_dri.so nouveau_vieux_dri.so rockchip_dri.so zink_dri.so kgsl_dri.so st7586_dri.so vc4_dri.so
     rm -Rf /usr/share/locale/*
