ec0c02e0e815984dd6ea5e007e5a95089b9bc8cb
Raphael <68374617+raphaelscholle@users.noreply.github.com>
2023-11-01 22:46:40 +0100

    dumbfix zero3 (needs to be undone after new kernel build), debug x20

--- build.sh
+++ build.sh
@@ -199,7 +199,9 @@ if [[ "${OS}" == "raspbian" ]]; then
 ${SCRIPT_DIR}/pishrink.sh -v ${PREV_WORK_DIR}/*.img
 elif [[ "${OS}" == "debian-X20" ]]; then
 chmod +x ${SCRIPT_DIR}/armbianshrink.sh
-${SCRIPT_DIR}/armbianshrink.sh -v ${PREV_WORK_DIR}/*.img ${PREV_WORK_DIR}/*.img1
+ls -a ${PREV_WORK_DIR}
+echo ${PREV_WORK_DIR}
+${SCRIPT_DIR}/armbianshrink.sh -s ${PREV_WORK_DIR}/*.img ${PREV_WORK_DIR}/*.img1
 img_filename="${*.img1}.img"
 cat img_filename
 mv *.img1 img_filename
