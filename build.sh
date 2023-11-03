3839333423e41d8f8babde170edda4d64da680d3
Raphael <68374617+raphaelscholle@users.noreply.github.com>
2023-11-03 15:14:55 +0100

    Update build.sh

--- build.sh
+++ build.sh
@@ -195,7 +195,7 @@ done
 log ""
 log "======================================================"
 log "Shrinking image: ${IMAGE_PATH_NAME}"
-if [[ "${OS}" == "raspbian" ]] || [[ "${OS}" == "debian-X20" ]] || [[ "${OS}" == "radxa-debian-rock-cm3" ]]; then
+if [[ "${OS}" == "raspbian" ]] || [[ "${OS}" == "debian-X20" ]]; then
 ${SCRIPT_DIR}/pishrink.sh -v ${PREV_WORK_DIR}/*.img
 else
 echo "This image can't be shrunken"
