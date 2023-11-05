8f05bc8edc2d82f1382b6968c7ebc34ce0898da4
Raphael <68374617+raphaelscholle@users.noreply.github.com>
2023-11-05 21:35:19 +0100

    Update initX20.sh

--- additionalFiles/initX20.sh
+++ additionalFiles/initX20.sh
@@ -3,10 +3,10 @@
 #initialise x20 air-unit
 
 #add platform identification
-mkdir -p /usr/local/share/openhd/platform/x20
-mkdir -p /usr/local/share/openhd/platform/wifi_card_type/88xxau/
-touch /usr/local/share/openhd/platform/wifi_card_type/88xxau/custom
-touch /usr/local/share/openhd/platform/x20/hdzero
+mkdir -p /usr/local/share/openhd_platform/x20
+mkdir -p /usr/local/share/openhd_platform/wifi_card_type/88xxau/
+touch /usr/local/share/openhd_platform/wifi_card_type/88xxau/custom
+touch /usr/local/share/openhd_platform/x20/hdzero
 
 sleep 10
 depmod -a
