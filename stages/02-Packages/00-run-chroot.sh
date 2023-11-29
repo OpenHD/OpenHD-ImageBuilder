1f938899e4651af807cf440b09637bf36499fc68
Raphael <68374617+raphaelscholle@users.noreply.github.com>
2023-11-29 15:40:57 +0100

    Update 00-run-chroot.sh

--- stages/02-Packages/00-run-chroot.sh
+++ stages/02-Packages/00-run-chroot.sh
@@ -158,10 +158,10 @@ if [ ! -e emmc ]; then
     install_openhd
 else
     apt update
-    apt purge -y chromium-x11 desktop-base gcc* gnome* gstreamer* libavcodec58 libavformat58 libavfilter7 libcups2 libgstreamer* libopencv* libqt5* python3* rockchip* xserver* codium firefox* dkms sddm plymouth plasma-desktop kde* lightdm *xfce* chromium || true
+    apt purge -y --allow-remove-essential chromium-x11 desktop-base gcc* gnome* gstreamer* libavcodec58 libavformat58 libavfilter7 libcups2 libgstreamer* libopencv* libqt5* python3* rockchip* xserver* codium firefox* dkms sddm plymouth plasma-desktop kde* lightdm *xfce* chromium || true
     curl -1sLf 'https://dl.cloudsmith.io/public/openhd/dev-release/setup.deb.sh'| sudo -E bash
     apt install linux-image-5.10.160-radxa-rk356x
-    apt autoremove -y
+    apt autoremove -y --allow-remove-essential
     apt list --installed
     df -h
     cd /opt/additionalFiles/
