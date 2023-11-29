35817d5918b563d20529900d2c7dfa2665afd456
Raphael <68374617+raphaelscholle@users.noreply.github.com>
2023-11-29 05:05:20 +0100

    Update 00-run-chroot.sh

--- stages/02-Packages/00-run-chroot.sh
+++ stages/02-Packages/00-run-chroot.sh
@@ -157,7 +157,8 @@ ls -a
 if [ ! -e emmc ]; then
     install_openhd
 else
-    apt remove -y codium firefox* dkms sddm plymouth plasma-desktop kde* lightdm *xfce* chromium
+    apt purge -y codium firefox* dkms sddm plymouth plasma-desktop kde* lightdm *xfce* chromium || true
+    curl -1sLf 'https://dl.cloudsmith.io/public/openhd/dev-release/setup.deb.sh'| sudo -E bash
     apt install linux-image-5.10.160-radxa-rk356x
     apt autoremove -y
     apt list --installed | awk -F/ '/^\/\// { pkg = $1; getline; size = $1; sub(/[^0-9]/, "", size); print size, pkg }' | sort -nr
