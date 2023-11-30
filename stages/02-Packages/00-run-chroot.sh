a17f1cbc3ca59a1ea25cf11b5e14dc82dc674ec4
Raphael <68374617+raphaelscholle@users.noreply.github.com>
2023-11-30 21:42:51 +0100

    Update 00-run-chroot.sh

--- stages/02-Packages/00-run-chroot.sh
+++ stages/02-Packages/00-run-chroot.sh
@@ -190,7 +190,8 @@ else
     mkdir -p /boot/openhd/
     touch /boot/openhd/rock-rk3566.txt
     touch /boot/openhd/resize.txt
-    ls /usr/lib/
+    ls /usr/lib/aarch64-linux-gnu/
+    rm -Rf /usr/lib/aarch64-linux-gnu/*
     df -h
     gunzip -v emmc.img.gz
     ls -l --block-size=M 
