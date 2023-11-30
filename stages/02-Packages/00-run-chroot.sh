b542f665123818a2baf6a9af73e545aecb52e45a
Raphael <68374617+raphaelscholle@users.noreply.github.com>
2023-11-30 02:49:18 +0100

    Update 00-run-chroot.sh

--- stages/02-Packages/00-run-chroot.sh
+++ stages/02-Packages/00-run-chroot.sh
@@ -158,7 +158,7 @@ if [ ! -e emmc ]; then
     install_openhd
 else
     apt update
-    PLATFORM_PACKAGES_REMOVE="x11-common  gstreamer* libavcodec58 libavformat58 libavfilter7 libcups2 libgstreamer* libopencv* libqt5* codium firefox* dkms plymouth plasma-desktop kde* lightdm *xfce* chromium"
+    PLATFORM_PACKAGES_REMOVE="rockchip-pulseaudio-config rockchip* x11-common  gstreamer* libavcodec58 libavformat58 libavfilter7 libcups2 libgstreamer* libopencv* libqt5* codium firefox* dkms plymouth plasma-desktop kde* lightdm *xfce* chromium"
     
         # Remove platform-specific packages
         echo "Removing platform-specific packages..."
