639db7d0b7a69d3f792b7033cadeb9f490b63ea0
Raphael <68374617+raphaelscholle@users.noreply.github.com>
2024-01-11 02:00:45 +0100

    Various improvements and updates  (#278)

--- additionalFiles/initX86.sh
+++ additionalFiles/initX86.sh
@@ -6,3 +6,8 @@ if grep -q "AMD Custom APU 0405" /proc/cpuinfo; then
 else
     echo "Not running on a Steam Deck."
 fi
+
+depmod -a
+touch /boot/openhd/resize.txt
+sudo bash /usr/local/bin/openhd_resize_util.sh 53aa3d65-1043-49fa-8740-148cba90bbae 3
+rm /boot/openhd/resize.txt
--- additionalFiles/openhd_resize_util.sh
+++ additionalFiles/openhd_resize_util.sh
@@ -1,9 +1,11 @@
-#resize function for x86
+#resize function
+
 # Specify the UUID of the partition you want to resize
-PARTITION_UUID="53aa3d65-1043-49fa-8740-148cba90bbae"
+PARTITION_UUID=$1
+PARTNR=$2
 
 # Check if the resize.txt file exists
-if [ -f "/boot/openhd/openhd/resize.txt" ]; then
+if [ -f "/boot/openhd/openhd/resize.txt" ] || [ -f "/boot/openhd/resize.txt" ]; then
     # Find the device path using the UUID
     DEVICE_PATH=$(blkid -l -o device -t UUID="$PARTITION_UUID")
     MOUNT_POINT=$(echo "$DEVICE_PATH" | sed 's/[0-9]*$//')
@@ -13,9 +15,9 @@ if [ -f "/boot/openhd/openhd/resize.txt" ]; then
 
 fdisk "$MOUNT_POINT" <<EOF
 d
-3
+$PARTNR
 n
-3
+$PARTNR
 
 
 w
@@ -28,7 +30,10 @@ EOF
         resize2fs "/dev/disk/by-uuid/$PARTITION_UUID"
 
         echo "Partition resized and filesystem expanded."
+        if [ -f "/boot/openhd/openhd/resize.txt" ]; then
         rm -Rf /boot/openhd/openhd/resize.txt
+        if [ -f "/boot/openhd/resize.txt" ]; then
+        rm -Rf /boot/openhd/resize.txt
         reboot
     else
         echo "Partition with UUID $PARTITION_UUID not found."
