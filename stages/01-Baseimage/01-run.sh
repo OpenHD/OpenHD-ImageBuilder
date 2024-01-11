6befa2d4c6707b7799f59ffce1ec082509c6f65b
Raphael <68374617+raphaelscholle@users.noreply.github.com>
2024-01-11 20:40:44 +0100

    Update 01-run.sh

--- stages/01-Baseimage/01-run.sh
+++ stages/01-Baseimage/01-run.sh
@@ -1,7 +1,7 @@
 if [[ "${OS}" = debian-X20 ]]; then
 pushd ${STAGE_WORK_DIR}
 sudo fdisk -l IMAGE.img
-fdisk IMAGE.img -u -d 2
+echo -e "d 2\nw" | fdisk IMAGE.img
 sudo fdisk -l IMAGE.img
 echo "______________SWAP REMOVED________"
 popd
