7eb2429333882d2741edeafceaca5d9fb5c0cd4c
Raphael <68374617+raphaelscholle@users.noreply.github.com>
2024-03-03 13:04:41 +0100

    Revert "Update 00-run-chroot.sh"
    This reverts commit 47a92cb2f75a0df5317d62075b8ebd40f01e24fa.

--- stages/02-Packages/00-run-chroot.sh
+++ stages/02-Packages/00-run-chroot.sh
@@ -21,7 +21,7 @@ function install_raspbian_packages {
     sudo apt update && apt remove -y dkms
     BASE_PACKAGES="openhd-sys-utils openhd qopenhd apt-transport-https apt-utils open-hd-web-ui"
     PLATFORM_PACKAGES_HOLD="raspberrypi-kernel libraspberrypi-dev libraspberrypi-bin libraspberrypi0 libraspberrypi-doc raspberrypi-bootloader"
-    PLATFORM_PACKAGES_REMOVE="locales gdb librsvg2-2 guile-2.2-libs firmware-libertas gcc-10 nfs-common libcamera*"
+    PLATFORM_PACKAGES_REMOVE="locales gdb librsvg2-2 guile-2.2-libs firmware-libertas gcc-10 nfs-common libcamera* raspberrypi-kernel"
     PLATFORM_PACKAGES="firmware-atheros openhd-userland libseek-thermal libcamera-openhd openhd-qt openssh-server 88x2bu-rpi"
 }
 # Ubuntu-Rockship-specific code
