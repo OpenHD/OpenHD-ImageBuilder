9b54a737966f6c5029481698462d3608562d05cb
Raphael <68374617+raphaelscholle@users.noreply.github.com>
2024-03-21 00:05:26 +0100

    debug fstab

--- stages/03-Preconfiguration/00-run-chroot.sh
+++ stages/03-Preconfiguration/00-run-chroot.sh
@@ -179,8 +179,9 @@ echo "removed for now"
 #  rm -Rf /usr/share/locale/*
 #  rm -Rf /usr/local/share/openhd/video/sunxisrc_h264.json
  rm -Rf /etc/rc.local
+ rm -Rf /lib/modules/5.8.0/kernel/drivers/net/88XXau_wfb.ko
  echo "HdZero" >> /etc/modules-load.d/modules.conf
- cat fstab
+ cat /etc/fstab
  sed -i 's/c34bd5d7-bc89-4fa1-85b8-47954ecd28ee/9714ff09-1989-492f-a35e-29d9654c22d5/' /etc/fstab
 #  touch /etc/apt/sources.list
 #  apt update
