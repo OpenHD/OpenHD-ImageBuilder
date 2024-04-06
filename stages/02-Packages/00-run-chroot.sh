9ea5e0f499c1673e48417333af67561bdd77bad8
Raphael <68374617+raphaelscholle@users.noreply.github.com>
2024-04-06 03:23:00 +0200

    dangerous but let's see (tomorrow)

--- stages/02-Packages/00-run-chroot.sh
+++ stages/02-Packages/00-run-chroot.sh
@@ -136,7 +136,7 @@ function install_openhd {
     done
     #Cleapup
     apt autoremove -y
-    apt upgrade -y --allow-downgrades
+    #apt upgrade -y --allow-downgrades
 
     # Install platform-specific packages
     echo "Installing platform-specific packages..."
