d52021751d499080463ed2197081a69a8d7c2b57
Raphael <68374617+raphaelscholle@users.noreply.github.com>
2024-04-06 22:58:39 +0200

    x20 custom kernel

--- stages/02-Packages/00-run-chroot.sh
+++ stages/02-Packages/00-run-chroot.sh
@@ -11,7 +11,7 @@ set -e
 function install_x20_packages {
     rm -Rf /etc/apt/sources.list.d/*
     rm -Rf /etc/apt/sources.list
-    BASE_PACKAGES="openhd-x20 openhd-sys-utils rtl8812au-x20 encode-sunxi"
+    BASE_PACKAGES="openhd-x20 openhd-sys-utils rtl8812au-x20 encode-sunxi linux-image-5.8.0-g76b16b8ee"
     PLATFORM_PACKAGES_REMOVE="*boost* locales guile-2.2-libs network-manager"
     PLATFORM_PACKAGES=""
 }
