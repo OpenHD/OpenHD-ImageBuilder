070e3e1f33f7369290dfb62cf44e445465a4c5f3
Raphael <68374617+raphaelscholle@users.noreply.github.com>
2024-04-07 20:01:20 +0200

    Update 01-run-chroot.sh

--- stages/03-Preconfiguration/01-run-chroot.sh
+++ stages/03-Preconfiguration/01-run-chroot.sh
@@ -50,6 +50,7 @@ echo "removed for now"
 # sudo systemctl disable systemd-journald.service
 sudo systemctl enable temperature_guardian
 ls /usr/local/bin
+modprobe 88XXau_ohd
 fi
 
 if [[ "${OS}" == "radxa-debian-rock-cm3" ]]; then
