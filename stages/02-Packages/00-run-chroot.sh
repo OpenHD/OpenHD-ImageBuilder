24a4c774ef0967c690d118d7a0ff5e0c0211ec2c
Raphael <68374617+raphaelscholle@users.noreply.github.com>
2024-04-07 13:47:06 +0200

    Update 00-run-chroot.sh

--- stages/02-Packages/00-run-chroot.sh
+++ stages/02-Packages/00-run-chroot.sh
@@ -13,7 +13,7 @@ function install_x20_packages {
     rm -Rf /etc/apt/sources.list
     # rm -Rf /lib/modules/5.8.0/kernel/drivers/net/*.ko
     # cp /opt/additionalFiles/88XXau_ohd.ko /lib/modules/5.8.0/kernel/drivers/net/
-    BASE_PACKAGES="openhd-x20 openhd-sys-utils rtl8812au-x20 encode-sunxi linux-image-5.8.0-gf1787c955"
+    BASE_PACKAGES="openhd-x20 openhd-sys-utils encode-sunxi"
     PLATFORM_PACKAGES_REMOVE="*boost* locales guile-2.2-libs network-manager"
     PLATFORM_PACKAGES=""
 }
