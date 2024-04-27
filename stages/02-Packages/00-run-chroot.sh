bcd732867c04b99a42630a1a9421d318cb741cee
Raphael <68374617+raphaelscholle@users.noreply.github.com>
2024-04-27 23:23:10 +0200

    Revert "fix rock5 Nr2"
    This reverts commit f2aff61d04f0d57b7671fa0b36d462e87c4d6583.

--- stages/02-Packages/00-run-chroot.sh
+++ stages/02-Packages/00-run-chroot.sh
@@ -36,7 +36,6 @@ function install_radxa-ubuntu_packages {
 }
 
 function install_radxa-debian_packages {
-    rm -RF /etc/xdg/kdeglobals
     BASE_PACKAGES="openhd-sys-utils openhd qopenhd-rk3588 apt-transport-https apt-utils open-hd-web-ui"
     PLATFORM_PACKAGES_HOLD="8852bu-dkms 8852be-dkms task-rockchip radxa-system-config-rockchip linux-image-rock-5a linux-image-5.10.110-6-rockchip linux-image-5.10.110-11-rockchip"
     PLATFORM_PACKAGES_REMOVE="dkms sddm plymouth plasma-desktop kde*"
