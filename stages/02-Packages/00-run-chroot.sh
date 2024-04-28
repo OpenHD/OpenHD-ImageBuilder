d024b764521afc597f4f842245222340bae891ff
Raphael <68374617+raphaelscholle@users.noreply.github.com>
2024-04-28 03:55:44 +0200

    Update 00-run-chroot.sh

--- stages/02-Packages/00-run-chroot.sh
+++ stages/02-Packages/00-run-chroot.sh
@@ -38,7 +38,7 @@ function install_radxa-ubuntu_packages {
 function install_radxa-debian_packages {
     BASE_PACKAGES="openhd-sys-utils openhd qopenhd-rk3588 apt-transport-https apt-utils open-hd-web-ui"
     PLATFORM_PACKAGES_HOLD="8852bu-dkms 8852be-dkms task-rockchip radxa-system-config-rockchip linux-image-rock-5a linux-image-5.10.110-6-rockchip linux-image-5.10.110-11-rockchip"
-    PLATFORM_PACKAGES_REMOVE="dkms sddm plymouth plasma-desktop kde*"
+    PLATFORM_PACKAGES_REMOVE="sddm plymouth plasma-desktop kde*"
     PLATFORM_PACKAGES="net-tools linux-headers-5.10.110-radxa-rockchip linux-image-5.10.110-radxa-rockchip rockchip-iq-openhd-r5 rsync procps mpv camera-engine-rkaiq mpp-rk3566 fpv-rk3566"
 }
 function install_radxa-debian_packages_rk3566 {
