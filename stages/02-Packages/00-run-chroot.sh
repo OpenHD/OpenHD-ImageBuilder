c0fb5f2bbdd3a7530416c54a08ccb28cbf09eae0
Raphael <68374617+raphaelscholle@users.noreply.github.com>
2024-05-07 13:17:26 +0200

    Update 00-run-chroot.sh

--- stages/02-Packages/00-run-chroot.sh
+++ stages/02-Packages/00-run-chroot.sh
@@ -9,11 +9,12 @@ set -e
 
 # X20 specific code
 function install_x20_packages {
+    sudo apt install -y firmware-realtek
     rm -Rf /etc/apt/sources.list.d/*
     rm -Rf /etc/apt/sources.list
     BASE_PACKAGES="openhd-x20 encode-sunxi openhd-sys-utils rtl8812au-x20"
     PLATFORM_PACKAGES_REMOVE="*boost* locales guile-2.2-libs network-manager"
-    PLATFORM_PACKAGES="firmware-realtek"
+    PLATFORM_PACKAGES=""
 }
 
 # Raspbian-specific code
