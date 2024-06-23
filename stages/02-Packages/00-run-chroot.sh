092d8f99bc1d0e324d993af370858ecf52d0b1af
Raphael <68374617+raphaelscholle@users.noreply.github.com>
2024-06-24 01:32:13 +0200

    Update 00-run-chroot.sh

--- stages/02-Packages/00-run-chroot.sh
+++ stages/02-Packages/00-run-chroot.sh
@@ -1,2 +1,28 @@
-sudo apt update
-sudo apt install -y -o Dpkg::Options::="--force-overwrite" --no-install-recommends --allow-downgrades openhd qopenhd-rk3566 linux-image-5.10.160-radxa-rk356x 
+#!/bin/bash
+
+# Update the package list
+sudo apt-get update
+
+# Get all versions of the libc6 package
+versions=$(apt-cache madison libc6 | awk '{print $3}')
+
+# Create a directory to store the downloaded packages
+mkdir -p libc6_packages
+cd libc6_packages
+
+# Loop through each version and download the package with dependencies
+for version in $versions; do
+    echo "Downloading libc6 version: $version"
+    apt-get download libc6=$version
+
+    # Get the dependencies for the specific version of libc6
+    dependencies=$(apt-cache depends libc6=$version | grep "Depends:" | awk '{print $2}')
+
+    # Download each dependency
+    for dependency in $dependencies; do
+        echo "Downloading dependency: $dependency"
+        apt-get download $dependency
+    done
+done
+
+echo "Download complete. All packages are stored in the libc6_packages directory."
