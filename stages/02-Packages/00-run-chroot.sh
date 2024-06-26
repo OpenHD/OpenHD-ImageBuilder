417800cebd2d9c0604bf8bf02735f90e8e9692c3
Raphael <68374617+raphaelscholle@users.noreply.github.com>
2024-06-26 21:14:17 +0200

    x20-hotfix

--- stages/02-Packages/00-run-chroot.sh
+++ stages/02-Packages/00-run-chroot.sh
@@ -1,7 +1,3 @@
 #!/bin/bash
 
-sudo apt-get update
-apt download librga2
-ls -a
-cp *.deb /home/openhd/
-ls -a
+rm -Rf /etc/modprobe.d/realtek_88XXau.conf
--- stages/03-Preconfiguration/99-run-chroot.sh
+++
@@ -1,6 +0,0 @@
-# Do this to the WORK folder of this stage
-# Make version number visible, rename fat partition
-MNT_DIR="${STAGE_WORK_DIR}/mnt"
-
-
-#return
