4a1bc0e31019d725fd7e8b550abd7bf750448c1c
Raphael <68374617+raphaelscholle@users.noreply.github.com>
2024-06-30 23:03:12 +0200

    Update 01-run.sh

--- stages/01-Baseimage/01-run.sh
+++ stages/01-Baseimage/01-run.sh
@@ -1,13 +1,5 @@
 # Extend Image Size
-ls -a ../../
-if [ ! -e ../../emmc ]; then
-    if [[ "${OS}" == radxa-debian-rock5a ]] || [[ "${OS}" == radxa-debian-rock5b ]];then
-    WANTEDSIZE="6800000000"
-    else
-    WANTEDSIZE="5632000000"
-    fi
-else
-WANTEDSIZE="10500000256"
+WANTEDSIZE="7500000256"
 echo "_______________________________________________________________________"
 echo "this is the emmc-model"
 echo "_______________________________________________________________________"
