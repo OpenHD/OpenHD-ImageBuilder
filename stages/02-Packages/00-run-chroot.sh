3d66fb198d38e70dc0f774072a2fe52d24411221
Raphael <68374617+raphaelscholle@users.noreply.github.com>
2024-06-30 02:16:26 +0200

    testing

--- stages/02-Packages/00-run-chroot.sh
+++ stages/02-Packages/00-run-chroot.sh
@@ -1,3 +1,12 @@
 #!/bin/bash
 
-echo "HELLO WORLD"
+wget https://fra1.digitaloceanspaces.com/openhd-images/Downloader/release/2.6/OpenHD-image-radxa-zero3w--release-06-26-2024-00-28-55.img.xz
+unxz OpenHD-image
+rm -Rf *.xz
+mv *.img /opt/emmc.img
+curl -1sLf \
+  'https://dl.cloudsmith.io/public/openhd/release/setup.deb.sh' \
+  | sudo -E bash
+
+sudo apt install -y openhd-sys-utils
+echo "______________________--DONE-______________________"
