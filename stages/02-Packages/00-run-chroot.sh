49d3ceef46f6831b91615452103ccb2421c1b04f
Raphael <68374617+raphaelscholle@users.noreply.github.com>
2024-06-30 22:08:30 +0200

    Update 00-run-chroot.sh

--- stages/02-Packages/00-run-chroot.sh
+++ stages/02-Packages/00-run-chroot.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 wget https://fra1.digitaloceanspaces.com/openhd-images/Downloader/release/2.6/OpenHD-image-radxa-zero3w--release-06-26-2024-00-28-55.img.xz
-unxz OpenHD-image
+ls -a
+unxz *.xz
 rm -Rf *.xz
 mv *.img /opt/emmc.img
 curl -1sLf \
