0c5710e9c255d84a25ac2f2bd297a4fdbd20f3fc
Raphael <68374617+raphaelscholle@users.noreply.github.com>
2024-07-01 01:42:45 +0200

    Update 00-run-chroot.sh

--- stages/02-Packages/00-run-chroot.sh
+++ stages/02-Packages/00-run-chroot.sh
@@ -13,7 +13,7 @@ sudo apt install -y openhd-sys-utils
 #Remove firstrun scripts
 rm -Rf /usr/lib/armbian/armbian-firstrun
 rm -Rf /root/.not_logged_in_yet
-sudo echo "openhd_emmc_util.sh flash debug" >> /root/.bashrc
+sudo echo "openhd_emmc_util.sh flash debug" >> /etc/profile
 #Autologin
 sudo sed -i 's/^ExecStart=.*/ExecStart=-\/sbin\/agetty --autologin root --noclear %I $TERM/' /lib/systemd/system/getty@.service
 
