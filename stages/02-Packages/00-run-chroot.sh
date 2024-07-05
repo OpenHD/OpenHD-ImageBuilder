5a466647476ac47d6669eb568cded07af4049cf8
Raphael <68374617+raphaelscholle@users.noreply.github.com>
2024-07-05 20:22:50 +0200

    testing x20

--- stages/02-Packages/00-run-chroot.sh
+++ stages/02-Packages/00-run-chroot.sh
@@ -10,6 +10,7 @@ set -e
 # X20 specific code
 function install_x20_packages {
     #sudo apt install -y firmware-realtek NEEDS FIXING
+    sudo apt install -y whiptail
     rm -Rf /etc/apt/sources.list.d/*
     rm -Rf /etc/apt/sources.list
     BASE_PACKAGES="openhd-x20 encode-sunxi openhd-sys-utils rtl8812au-x20"
