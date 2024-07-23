9250efafacba7fa36f512a39e8e42940de6f4e21
Raphael <68374617+raphaelscholle@users.noreply.github.com>
2024-07-23 05:11:59 +0200

    resize tests

--- stages/03-Preconfiguration/02-run-chroot.sh
+++ stages/03-Preconfiguration/02-run-chroot.sh
@@ -16,8 +16,9 @@ elif [[ "${OS}" == "radxa-debian-rock5b" ]]; then
 mkdir -p /usr/local/share/openhd_platform/rock/rock5b
 touch /boot/openhd/rock-5b.txt
 elif [[ "${OS}" == "radxa-debian-rock-cm3" ]]; then
-touch /boot/openhd/rock-rk3566.txt
+touch /conf/openhd/rock-rk3566.txt
 mkdir -p /usr/local/share/openhd/platform/rock/rk3566
+touch /conf/openhd/resize.txt
 elif [[ "${OS}" == "radxa-debian-rock-cm3-core3566" ]]; then
 touch /conf/openhd/rock-rk3566.txt
 touch /conf/openhd/resize.txt
