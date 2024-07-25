1eac38930c89aea060ef44ae4a860ef04cb22ddb
Raphael <68374617+raphaelscholle@users.noreply.github.com>
2024-07-25 22:11:21 +0200

    Update build.sh

--- build.sh
+++ build.sh
@@ -219,12 +219,7 @@ else
 dd if=/dev/zero of=fat.img bs=1M count=300
 cat fat.img >> ${PREV_WORK_DIR}/*.img
 rm -Rf fat.img
-#sgdisk -e ${PREV_WORK_DIR}/*.img
-log "______before fdisk_________________________________________________________debug1"
-
 echo -e "n\np\n3\n\n\n\nt\n3\nc\nw" | sudo fdisk ${PREV_WORK_DIR}/*.img
-log "_______________________________________________________________debug1"
-# sudo parted ${PREV_WORK_DIR}/*.img set 3 lba on
 log "Video Partition Added"
 fi
 
