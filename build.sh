b0bb3344abcd458eeb27d05c278a50af9e207d51
Raphael <68374617+raphaelscholle@users.noreply.github.com>
2024-07-25 19:23:31 +0200

    Update build.sh

--- build.sh
+++ build.sh
@@ -215,11 +215,11 @@ sgdisk -e ${PREV_WORK_DIR}/*.img
 echo -e "n\n4\n\n\n\n0C00\nw\ny" | sudo gdisk ${PREV_WORK_DIR}/*.img
 sudo parted ${PREV_WORK_DIR}/*.img set 4 msftdata on
 log "Video Partition Added"
-else 
+else
 dd if=/dev/zero of=fat.img bs=1M count=300
 cat fat.img >> ${PREV_WORK_DIR}/*.img
 rm -Rf fat.img
-sgdisk -e ${PREV_WORK_DIR}/*.img
+sudo sgdisk --zap-all ${PREV_WORK_DIR}/*.img
 echo -e "n\np\n3\n\n\n\nt\n3\nc\nw" | sudo fdisk ${PREV_WORK_DIR}/*.img
 sudo parted ${PREV_WORK_DIR}/*.img set 3 lba on
 log "Video Partition Added"
