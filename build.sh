e412f81b922d20494d73792e2f2ef84298d9a3ef
Raphael <68374617+raphaelscholle@users.noreply.github.com>
2024-07-25 14:24:57 +0200

    Update build.sh

--- build.sh
+++ build.sh
@@ -213,6 +213,7 @@ cat fat.img >> ${PREV_WORK_DIR}/*.img
 rm -Rf fat.img
 sgdisk -e ${PREV_WORK_DIR}/*.img
 echo -e "n\n4\n\n\n\n0C00\nw\ny" | sudo gdisk ${PREV_WORK_DIR}/*.img
+sudo parted ${PREV_WORK_DIR}/*.img set 4 msftdata on
 ls -s
 log "Video Partition Added"
 fi
