208987c43f86c69df370c4ab0bada79fc354e1d6
Raphael <68374617+raphaelscholle@users.noreply.github.com>
2024-07-25 21:52:55 +0200

    Update build.sh

--- build.sh
+++ build.sh
@@ -219,7 +219,9 @@ else
 dd if=/dev/zero of=fat.img bs=1M count=300
 cat fat.img >> ${PREV_WORK_DIR}/*.img
 rm -Rf fat.img
-sgdisk -e ${PREV_WORK_DIR}/*.img
+#sgdisk -e ${PREV_WORK_DIR}/*.img
+log "______before fdisk_________________________________________________________debug1"
+
 echo -e "n\np\n3\n\n\n\nt\n3\nc\nw" | sudo fdisk ${PREV_WORK_DIR}/*.img
 log "_______________________________________________________________debug1"
 sudo parted -g ${PREV_WORK_DIR}/*.img set 3 lba on
