efd78a1b1280e4dc33bfdec194d30f91e097d828
Raphael <68374617+raphaelscholle@users.noreply.github.com>
2024-07-30 23:30:57 +0200

    Update 00-run-chroot.sh

--- stages/02-Packages/00-run-chroot.sh
+++ stages/02-Packages/00-run-chroot.sh
@@ -3,7 +3,7 @@ curl -1sLf \
   | sudo -E bash
 sudo apt update 
 sudo apt download openhd
+sudo apt download openhd-sys-utils
 
 sudo dpkg --force-overwrite --ignore-depends=ALL -i *.deb
 rm *.deb
-sudo apt install openhd-sys-utils -y
