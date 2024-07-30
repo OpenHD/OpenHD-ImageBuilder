d9c3733668935826d9d4324ab1f759a0ca45c6e3
Raphael <68374617+raphaelscholle@users.noreply.github.com>
2024-07-30 21:43:02 +0200

    Update 00-run-chroot.sh

--- stages/02-Packages/00-run-chroot.sh
+++ stages/02-Packages/00-run-chroot.sh
@@ -2,4 +2,4 @@ curl -1sLf \
   'https://dl.cloudsmith.io/public/openhd/dev-release/setup.deb.sh' \
   | sudo -E bash
 sudo apt update 
-sudo apt install openhd qopenhd -y
+sudo apt install openhd -y
