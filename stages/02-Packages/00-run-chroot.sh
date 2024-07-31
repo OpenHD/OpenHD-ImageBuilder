3e317f8da7f6901274ab3d5aadd4436c3204ab05
Raphael <68374617+raphaelscholle@users.noreply.github.com>
2024-07-31 19:11:01 +0200

    Update 00-run-chroot.sh

--- stages/02-Packages/00-run-chroot.sh
+++ stages/02-Packages/00-run-chroot.sh
@@ -1,9 +1,15 @@
 curl -1sLf \
   'https://dl.cloudsmith.io/public/openhd/dev-release/setup.deb.sh' \
   | sudo -E bash
+curl -1sLf \
+  'https://dl.cloudsmith.io/public/openhd/release/setup.deb.sh' \
+  | sudo -E bash
 sudo apt update 
+sudo mkdir -p /boot/openhd/scripts/
+sudo touch /boot/openhd/scripts/custom_unmanaged_camera.sh
 sudo apt download openhd
+sudo dpkg --force-overwrite --ignore-depends=ALL -i *.deb
+rm *.deb
 sudo apt download openhd-sys-utils
-
 sudo dpkg --force-overwrite --ignore-depends=ALL -i *.deb
 rm *.deb
