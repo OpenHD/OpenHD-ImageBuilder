37571cc992619c2c2a25b187119ab4157815382c
Raphael <68374617+raphaelscholle@users.noreply.github.com>
2024-08-04 23:54:52 +0200

    Update 00-run-chroot.sh

--- stages/03-Preconfiguration/00-run-chroot.sh
+++ stages/03-Preconfiguration/00-run-chroot.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
+
+echo "_____________________________________________"
+gst-inspect-1.0 mpph264enc  
+echo "_____________________________________________"
 # create a user account that should be the same on all platforms
 USERNAME="openhd"
 PASSWORD="openhd"
