7e9a4b0946507d3ac8efe50150c9b709abb4cd97
Raphael <68374617+raphaelscholle@users.noreply.github.com>
2024-08-27 23:16:06 +0200

    Update 00-run-chroot.sh

--- stages/03-Preconfiguration/00-run-chroot.sh
+++ stages/03-Preconfiguration/00-run-chroot.sh
@@ -192,7 +192,7 @@ if [[ "${OS}" == "debian-X20" ]]; then
  echo "HdZero" >> /etc/modules-load.d/modules.conf
  sudo sed -i '/^\/dev\/mmcblk0p2/d' /etc/fstab
  sudo sed -i 's/,commit=600//g' /etc/fstab
- sudo echo "UUID=1A7D-9881  /external  auto  defaults  0  2" | sudo tee -a /etc/fstab
+ sudo echo "UUID=41ED-7F73  /external  auto  defaults  0  2" | sudo tee -a /etc/fstab
  sudo echo "UUID=e6c9676e-0cbc-41d4-8142-7d08a515c244  none  swap  sw  0  0" | sudo tee -a /etc/fstab
  sudo sed -i 's/c34bd5d7-bc89-4fa1-85b8-47954ecd28ee/9714ff09-1989-492f-a35e-29d9654c22d5/g' /etc/fstab
  sudo echo "while true; do journalctl > /boot/log_$(date +"\%Y-\%m-\%d_\%H-\%M-\%S").txt && sleep 120 || journalctl > /boot/log.txt && sleep 120; done" >> /root/.bashrc
