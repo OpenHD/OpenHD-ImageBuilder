f7ffa85ef8798542b1b6965d765f3f1137dfa4e5
Raphael <68374617+raphaelscholle@users.noreply.github.com>
2025-01-11 19:41:24 +0100

    testing stuff

--- stages/03-Preconfiguration/02-run-chroot.sh
+++ stages/03-Preconfiguration/02-run-chroot.sh
@@ -7,7 +7,8 @@
 
 rm -Rf /opt/additionalFiles
 echo "alias led='led_sys.sh'" | sudo tee -a /etc/bash.bashrc >/dev/null
-
+#create ramdisk
+sudo mkdir -p /ramdisk && echo "tmpfs /ramdisk tmpfs defaults,size=100M 0 0" | sudo tee -a /etc/fstab && sudo mount /ramdisk
 
 if [[ "${OS}" == "radxa-debian-rock5a" ]]; then
 mkdir -p /usr/local/share/openhd_platform/rock/rock5a
