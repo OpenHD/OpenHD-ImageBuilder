225b3610e8631453eba8204bb0414d16b8a090e2
Raphael <68374617+raphaelscholle@users.noreply.github.com>
2025-01-19 20:07:14 +0100

    Update common.sh

--- scripts/common.sh
+++ scripts/common.sh
@@ -7,6 +7,8 @@ mount_image () {
     IMG_FILE="${STAGE_WORK_DIR}/IMAGE.img"
 
     log "Mounting image file: ${IMG_FILE}"
+    log "DEBUG__________"
+    echo -e 'v\nx\ne\nv\nw\ny\n' | gdisk IMAGE.img
 
     PARTED_OUT=$(parted -s "${IMG_FILE}" unit b print)
 
