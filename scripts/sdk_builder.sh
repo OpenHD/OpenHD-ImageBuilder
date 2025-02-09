014723d8f1634618e639dfd02015c864a50a9796
Raphael <raphael@openhdfpv.org>
2025-02-09 18:52:16 +0100

    Update sdk_builder.sh

--- scripts/sdk_builder.sh
+++ scripts/sdk_builder.sh
@@ -41,6 +41,7 @@
 
 CONFIG_DIR="images"
 
+sudo apt-get install -y git libpoco-dev bison bcc ssh make gcc gcc-multilib g++-multilib module-assistant expect g++ gawk texinfo libssl-dev bison flex fakeroot cmake unzip gperf autoconf device-tree-compiler libncurses5-dev pkg-config bc python-is-python3 passwd openssl openssh-server openssh-client vim file cpio rsync
 ###############################################################################
 # 1) Helper function: list available config files
 ###############################################################################
