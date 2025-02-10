22ba0c2a9eb2a728faf19828e394adc90ec6a2ce
Raphael <raphael@openhdfpv.org>
2025-02-10 19:23:58 +0100

    Update sdk_builder.sh

--- scripts/sdk_builder.sh
+++ scripts/sdk_builder.sh
@@ -127,12 +127,12 @@ perform_build() {
   cd ../../../../../../../
 
   echo "adding OpenHD and updating Poco"
-  cp -rfv ../additionalFiles/Buildroot/openhd sysdrv/source/buildroot/buildroot*/package/
-  rm -Rf sysdrv/source/buildroot/buildroot*/package/poco
-  cp -rfv ../additionalFiles/Buildroot/UpdatedDependencies/poco sysdrv/source/buildroot/buildroot*/package/poco
+  cp -rfv ../additionalFiles/Buildroot/openhd buildroot/sysdrv/source/buildroot/buildroot*/package/
+  rm -Rf buildroot/sysdrv/source/buildroot/buildroot*/package/poco
+  cp -rfv ../additionalFiles/Buildroot/UpdatedDependencies/poco buildroot/sysdrv/source/buildroot/buildroot*/package/poco
   echo "adding OpenHD configuation option"
-  sed -i '/menu "Audio and video applications"/a\        source "package/openhd/Config.in"' sysdrv/source/buildroot/buildroot*/package/Config.in
-  echo -e "\n# Enable build OpenHD\nCONFIG_SYSDRV_ENABLE_OPENHD=y\n\$(eval \$(call MACRO_CHECK_ENABLE_PKG, RK_ENABLE_OPENHD))" >> sysdrv/source/cfg/package.mk
+  sed -i '/menu "Audio and video applications"/a\        source "package/openhd/Config.in"' buildroot/sysdrv/source/buildroot/buildroot*/package/Config.in
+  echo -e "\n# Enable build OpenHD\nCONFIG_SYSDRV_ENABLE_OPENHD=y\n\$(eval \$(call MACRO_CHECK_ENABLE_PKG, RK_ENABLE_OPENHD))" >> buildroot/sysdrv/source/cfg/package.mk
 
 exit
 ./build.sh lunch <<EOF
