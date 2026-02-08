deac27e9fd207091fcc6f8739609b6a5647e9a30
Raphael <68374617+raphaelscholle@users.noreply.github.com>
2026-02-08 23:31:34 +0100

    Update update.sh

--- stages/Update/update.sh
+++ stages/Update/update.sh
@@ -80,6 +80,14 @@ if [[ "${OS}" == "raspbian" ]]; then
   $APT remove openhd-linux-pi
   echo "Installing custom Kernel package"
   $APT install openhd-linux-pi
+  # Fix Arducam Pivariety driver + imx662
+  curl -s --compressed "https://arducam.github.io/arducam_ppa/KEY.gpg" | sudo apt-key add -
+            sudo curl -s --compressed -o /etc/apt/sources.list.d/arducam_list_files.list "https://arducam.github.io/arducam_ppa/arducam_list_files.list"
+            sudo apt update
+            apt-cache madison arducam-pivariety-sdk-dev
+  sudo apt install -y \
+  -o Dpkg::Options::=--force-overwrite \
+  arducam-pivariety-sdk-dev=1.0.5
 fi
 
 if [[ "${OS}" == "radxa-debian-cubie" ]]; then
