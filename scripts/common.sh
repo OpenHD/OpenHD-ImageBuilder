9bacfd8e541d9a54f00a486efd73928c3b58274c
Raphael <68374617+raphaelscholle@users.noreply.github.com>
2026-05-27 12:40:51 +0200

    Bind chroot tmp outside target image

--- scripts/common.sh
+++ scripts/common.sh
@@ -151,11 +151,9 @@ on_chroot() {
         mount --bind /etc/resolv.conf "${MNT_DIR}/etc/resolv.conf"
     fi
 
-    if [[ "${OPENHD_LITE_IMAGE:-false}" == "true" ]]; then
-        mkdir -p "${CHROOT_TMP_DIR}" "${MNT_DIR}/tmp"
-        chmod 1777 "${CHROOT_TMP_DIR}" "${MNT_DIR}/tmp"
-        mountpoint -q "${MNT_DIR}/tmp" || mount --bind "${CHROOT_TMP_DIR}" "${MNT_DIR}/tmp"
-    fi
+    mkdir -p "${CHROOT_TMP_DIR}" "${MNT_DIR}/tmp"
+    chmod 1777 "${CHROOT_TMP_DIR}" "${MNT_DIR}/tmp"
+    mountpoint -q "${MNT_DIR}/tmp" || mount --bind "${CHROOT_TMP_DIR}" "${MNT_DIR}/tmp"
 
     if [ ! -e ${MNT_DIR}/opt/additionalFiles ]; then
     df -h
